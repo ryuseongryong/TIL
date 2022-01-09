2f321a898940610135682ffc4f0fb2c795d1c208
ryuseongryong <ryuseongryong@gmail.com>
2022-01-09 23:27:57 +0900

    nodejs study day three

---
+++ DreamCoding/NPM/app.js
@@ -0,0 +1,9 @@
+console.log('hello!');
+console.log('안녕!');
+console.log('안녕하세요~');
+
+const _ = require('underscore');
+
+// no more useful
+const array = [1, 2];
+_.forEach(array, (num) => console.log(num));
