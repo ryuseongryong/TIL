635c267e489875d701de01c7939a317f0a826a5f
ryuseongryong <ryuseongryong@gmail.com>
2022-01-20 23:42:09 +0900

    nodejs study day fourteen - MySQL DB

--- DreamCoding/7-Dwitter/server/data/auth.js
+++ DreamCoding/7-Dwitter/server/data/auth.js
@@ -1,34 +1,31 @@
-let users = [
-  {
-    id: '1',
-    username: 'bob',
-    password: '$2b$12$B2i8k/1mnnNGpP3g4PIHkuoZq78vrF6n45pZNAxQuwrqDhJqFGrnG',
-    // password: '12345',
-    name: 'Bob',
-    email: 'bob@gmail.com',
-    url: 'https://m.media-amazon.com/images/M/MV5BNzg0MWEyZjItOTZlMi00YmRjLWEyYzctODIwMDU0OThiMzNkXkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_UY317_CR13,0,214,317_AL_.jpg',
-  },
-  {
-    id: '2',
-    username: 'seongryong',
-    password: '$2b$12$B2i8k/1mnnNGpP3g4PIHkuoZq78vrF6n45pZNAxQuwrqDhJqFGrnG',
-    // password: '12345',
-    name: 'Seongryong',
-    email: 'ryuseongryong@gmail.com',
-    url: 'https://avatars.githubusercontent.com/u/58920833?s=400&u=ac2997091b3a84b129630b576fddccaaadabecc4&v=4',
-  },
-];
+//     username: 'bob',
+//     url: 'https://m.media-amazon.com/images/M/MV5BNzg0MWEyZjItOTZlMi00YmRjLWEyYzctODIwMDU0OThiMzNkXkEyXkFqcGdeQXVyNjUxMjc1OTM@._V1_UY317_CR13,0,214,317_AL_.jpg',
+//     username: 'seongryong',
+//     url: 'https://avatars.githubusercontent.com/u/58920833?s=400&u=ac2997091b3a84b129630b576fddccaaadabecc4&v=4',
+
+import { db } from '../db/database.js';
 
 export async function findByUsername(username) {
-  return users.find((user) => user.username === username);
+  return db
+    .execute('SELECT * FROM users WHERE username=?', [username])
+    .then((result) => result[0][0]);
 }
 
 export async function findById(id) {
-  return users.find((user) => user.id === id);
+  return db
+    .execute('SELECT * FROM users WHERE id=?', [id])
+    .then((result) => result[0][0]);
 }
 
 export async function createUser(user) {
-  const created = { ...user, id: Date.now().toString() };
-  users.push(created);
-  return created.id;
+  const { username, password, name, email, url } = user;
+  return db
+    .execute(
+      'INSERT INTO users (username, password, name, email, url) VALUES (?, ?, ?, ?, ?)',
+      [username, password, name, email, url]
+    )
+    .then((result) => {
+      console.log(result[0].insertId);
+      return result;
+    });
 }
--- DreamCoding/7-Dwitter/server/data/tweet.js
+++ DreamCoding/7-Dwitter/server/data/tweet.js
@@ -1,65 +1,42 @@
-import * as userRepository from './auth.js';
+import { db } from '../db/database.js';
 
-let tweets = [
-  {
-    id: '1',
-    text: '성룡이 화이팅!',
-    createdAt: new Date().toString(),
-    userId: '1',
-  },
-  {
-    id: '2',
-    text: '드림코딩에서 강의 들으면 너무 좋으다',
-    createdAt: new Date().toString(),
-    userId: '2',
-  },
-];
-
+const SELECT_JOIN =
+  'SELECT tw.id, tw.text, tw.createdAt, tw.userId, us.username, us.name, us.url FROM tweets as tw JOIN users as us ON tw.userId=us.id';
+const ORDER_DESC = 'ORDER BY tw.createdAt DESC';
 export async function getAll() {
-  return Promise.all(
-    tweets.map(async (tweet) => {
-      const { username, name, url } = await userRepository.findById(
-        tweet.userId
-      );
-      return { ...tweet, username, name, url };
-    })
-  );
+  return db
+    .execute(`${SELECT_JOIN} ${ORDER_DESC}`) //
+    .then((result) => result[0]);
 }
 
 export async function getAllByUsername(username) {
-  return getAll().then((tweets) => {
-    return tweets.filter((tweet) => tweet.username === username);
-  });
+  return db
+    .execute(`${SELECT_JOIN} WHERE username=? ${ORDER_DESC}`, [username]) //
+    .then((result) => result[0]);
 }
 
 export async function getById(id) {
-  const found = tweets.find((tweet) => (tweet.id = id));
-  if (!found) {
-    return null;
-  }
-  const { username, name, url } = await userRepository.findById(found.userId);
-  return { ...found, username, name, url };
+  return db
+    .execute(`${SELECT_JOIN} WHERE tw.id=?`, [id])
+    .then((result) => result[0][0]);
 }
 
 export async function create(text, userId) {
-  const tweet = {
-    id: Date.now().toString(),
-    text,
-    createdAt: new Date(),
-    userId,
-  };
-  tweets = [tweet, ...tweets];
-  return getById(tweet.id);
+  return db
+    .execute('INSERT INTO tweets (text, createdAt, userId) VALUES (?, ?, ?)', [
+      text,
+      new Date(),
+      userId,
+    ])
+    .then((result) => getById(result[0].insertId));
 }
 
 export async function update(id, text) {
-  const tweet = tweets.find((tweet) => tweet.id === id);
-  if (tweet) {
-    tweet.text = text;
-  }
-  return getById(tweet.id);
+  return db
+    .execute('UPDATE tweets SET text=? WHERE id=?', [text, id])
+    .then(() => getById(id));
 }
 
 export async function remove(id) {
-  tweets = tweets.filter((tweet) => tweet.id !== id);
+  return db.execute('DELETE FROM tweets WHERE id=?', [id]);
 }
