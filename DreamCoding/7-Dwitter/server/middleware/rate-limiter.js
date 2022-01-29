b5027726ca2b10d20ab277f3938afd6c4a159a51
ryuseongryong <ryuseongryong@gmail.com>
2022-01-29 22:23:28 +0900

    nodejs study day twenty three - express-rate-limiter2

--- DreamCoding/7-Dwitter/server/middleware/rate-limiter.js
+++ DreamCoding/7-Dwitter/server/middleware/rate-limiter.js
@@ -4,4 +4,6 @@ import { config } from '../config.js';
 export default rateLimit({
   windowMs: config.rateLimit.windowMs,
   max: config.rateLimit.maxRequest,
+  // 특정 IP 또는 project 명을 기재하면 글로벌하게 카운트할 수 있다.
+  keyGenerator: (req, res) => 'dwitter',
 });
