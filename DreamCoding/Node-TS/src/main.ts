5f5cc8f5d12c5595052af0de946257773fae741a
ryuseongryong <ryuseongryong@gmail.com>
2022-02-03 23:15:30 +0900

    nodejs study day 28 - typescript node

---
+++ DreamCoding/Node-TS/src/main.ts
@@ -0,0 +1,7 @@
+// ref : Typescript-Node-Starter(ms)
+console.log('Hello World~~');
+console.log('Hi HI HI ');
+for (let i = 0; i < 100; i++) {
+  console.log(i);
+}
+// # sourceMappingURL=main.js.map
