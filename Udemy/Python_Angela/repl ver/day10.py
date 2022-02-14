acff3806a19f5b9a94ff98c5c392a12873053a44
ryuseongryong <ryuseongryong@gmail.com>
2022-02-15 00:37:47 +0900

    python day10-1 : calculator

--- Udemy/Python_Angela/repl ver/day10.py
+++ Udemy/Python_Angela/repl ver/day10.py
@@ -2,6 +2,9 @@
 
 # Fucntion with Outputs
 
+from lib.calc_art import logo
+
+
 def format_name(f_name, l_name):
     new_f_name = ""
     new_l_name = ""
@@ -82,3 +85,56 @@ year = int(input("Enter a year: "))
 month = int(input("Enter a month: "))
 days = days_in_month(year, month)
 print(days)
+#
+print(logo)
+
+
+def add(n1, n2):
+    return n1 + n2
+
+
+def subtract(n1, n2):
+    return n1 - n2
+
+
+def multiply(n1, n2):
+    return n1 * n2
+
+
+def divide(n1, n2):
+    return n1 / n2
+
+
+operations = {
+    "+": add,
+    "-": subtract,
+    "*": multiply,
+    "/": divide
+}
+
+function = operations["+"]
+
+
+def calculator():
+    num1 = float(input("What's the first number?: "))
+    for symbol in operations:
+        print(symbol)
+
+    should_continue = True
+
+    while should_continue:
+        operation_symbol = input("Pick an operation from the line above: ")
+        num2 = float(input("What's the second number?: "))
+        calculation_function = operations[operation_symbol]
+        answer = calculation_function(num1, num2)
+
+        print(f"{num1} {operation_symbol} {num2} = {answer}")
+
+        if input(f"Type 'y' to continue calculating with {answer}, or type 'n' to start a new calculation: ") == "y":
+            num1 = answer
+        else:
+            should_continue = False
+            calculator()
+
+
+calculator()
