eca1f221ddfa78173035968e5c3cf73a86699d9c
ryuseongryong <ryuseongryong@gmail.com>
2022-02-20 22:08:47 +0900

    python day15 : coffeemachine-REF

--- Udemy/Python_Angela/pycharm ver/CoffeeMachine/main.py
+++ Udemy/Python_Angela/pycharm ver/CoffeeMachine/main.py
@@ -23,7 +23,6 @@ MENU = {
         "cost": 3.0,
     }
 }
-
 resources = {
     "water": 300,
     "milk": 200,
@@ -42,56 +41,126 @@ resources = {
 # TODO :  make coffee
 
 # Todo: 1. Prompt user by asking “What would you like? (espresso/latte/cappuccino):”
-# Todo: a. Check the user’s input to decide what to do next.
-# Todo: b. The prompt should show every time action has completed, e.g. once the drink is
-# Todo: dispensed. The prompt should show again to serve the next customer.
+#  a. Check the user’s input to decide what to do next.
+#  b. The prompt should show every time action has completed,
+#  e.g. once the drink is dispensed. The prompt should show again to serve the next customer.
+
+profit = 0
+
+
+def is_resource_sufficient(order_ingredients):
+    """Returns True when order can be made, False if ingredients are insufficient"""
+    is_enough = True
+    for item in order_ingredients:
+        if order_ingredients[item] >= resources[item]:
+            print(f"Sorry, there is not enough {item}.")
+            is_enough = False
+    return is_enough
+
+
+def process_coins():
+    """Returns the total calculated from coins inserted."""
+    print("Please insert coins.")
+    total = int(input("how many quarters?: ")) * 0.25
+    total += int(input("how many dimes?: ")) * 0.1
+    total += int(input("how many nickles?: ")) * 0.05
+    total += int(input("how many pennies?: ")) * 0.01
+    return total
+
+
+def is_transaction_successful(money_received, drink_cost):
+    """Return True when the payment is accepted, or False if money is insufficient"""
+    if money_received >= drink_cost:
+        change = round(money_received - drink_cost, 2)
+        print(f"Here is ${change} in change.")
+        global profit
+        profit += drink_cost
+        return True
+    else:
+        print("Sorry that's not enough money, Money refunded.")
+        return False
+
+
+def make_coffee(drink_name, order_ingredients):
+    """Deduct the required ingredients from the resources."""
+    for item in order_ingredients:
+        resources[item] -= order_ingredients[item]
+    print(f"Here is your {drink_name} ☕. enjoy!")
+
+is_on = True
+
+while is_on:
+    choice = input("What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+    else:
+        drink = MENU[choice]
+        ingredient = drink["ingredients"]
+        cost = drink["cost"]
+        if is_resource_sufficient(ingredient):
+            payment = process_coins()
+            if is_transaction_successful(payment, cost):
+                make_coffee(choice, ingredient)
+
+
 # Todo: 2. Turn off the Coffee Machine by entering “off” to the prompt.
-# Todo: a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off the machine. Your code should end execution when this happens.
+#  a. For maintainers of the coffee machine, they can use “off” as the secret word to turn off the machine.
+#  Your code should end execution when this happens.
+
 # Todo: 3. Print report.
-# Todo: a. When the user enters “report” to the prompt, a report should be generated that shows
-# Todo: the current resource values. e.g.
-# Todo: Water: 100ml
-# Todo: Milk: 50ml
-# Todo: Coffee: 76g
-# Todo: Money: $2.5
+#  a. When the user enters “report” to the prompt, a report should be generated that shows
+#  the current resource values. e.g.
+#  Water: 100ml
+#  Milk: 50ml
+#  Coffee: 76g
+#  Money: $2.5
+
 # Todo: 4. Check resources sufficient?
-# Todo: a. When the user chooses a drink, the program should check if there are enough
-# Todo: resources to make that drink.
-# Todo: b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should
-# Todo: not continue to make the drink but print: “Sorry there is not enough water.”
-# Todo: c. The same should happen if another resource is depleted, e.g. milk or coffee.
+#  a. When the user chooses a drink, the program should check if there are enough
+#  resources to make that drink.
+#  b. E.g. if Latte requires 200ml water but there is only 100ml left in the machine. It should
+#  not continue to make the drink but print: “Sorry there is not enough water.”
+#  c. The same should happen if another resource is depleted, e.g. milk or coffee.
+
 # Todo: 5. Process coins.
-# Todo: a. If there are sufficient resources to make the drink selected, then the program should
-# Todo: prompt the user to insert coins.
-# Todo: b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
-# Todo: c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
-# Todo: pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
+#  a. If there are sufficient resources to make the drink selected, then the program should
+#  prompt the user to insert coins.
+#  b. Remember that quarters = $0.25, dimes = $0.10, nickles = $0.05, pennies = $0.01
+#  c. Calculate the monetary value of the coins inserted. E.g. 1 quarter, 2 dimes, 1 nickel, 2
+#  pennies = 0.25 + 0.1 x 2 + 0.05 + 0.01 x 2 = $0.52
+
 # Todo: 6. Check transaction successful?
-# Todo: a. Check that the user has inserted enough money to purchase the drink they selected.
-# Todo: E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the
-# Todo: program should say “Sorry that's not enough money. Money refunded.”.
-# Todo: b. But if the user has inserted enough money, then the cost of the drink gets added to the
-# Todo: machine as the profit and this will be reflected the next time “report” is triggered. E.g.
-# Todo: Water: 100ml
-# Todo: Milk: 50ml
-# Todo: Coffee: 76g
-# Todo: Money: $2.5
-# Todo: c. If the user has inserted too much money, the machine should offer change.
-# Todo: E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal
-# Todo: places.
+#  a. Check that the user has inserted enough money to purchase the drink they selected.
+#  E.g Latte cost $2.50, but they only inserted $0.52 then after counting the coins the
+#  program should say “Sorry that's not enough money. Money refunded.”.
+#  b. But if the user has inserted enough money, then the cost of the drink gets added to the
+#  machine as the profit and this will be reflected the next time “report” is triggered. E.g.
+#  Water: 100ml
+#  Milk: 50ml
+#  Coffee: 76g
+#  Money: $2.5
+#  c. If the user has inserted too much money, the machine should offer change.
+#  E.g. “Here is $2.45 dollars in change.” The change should be rounded to 2 decimal places.
+
 # Todo: 7. Make Coffee.
-# Todo: a. If the transaction is successful and there are enough resources to make the drink the
-# Todo: user selected, then the ingredients to make the drink should be deducted from the
-# Todo: coffee machine resources.
-# Todo: E.g. report before purchasing latte:
-# Todo: Water: 300ml
-# Todo: Milk: 200ml
-# Todo: Coffee: 100g
-# Todo: Money: $0
-# Todo: Report after purchasing latte:
-# Todo: Water: 100ml
-# Todo: Milk: 50ml
-# Todo: Coffee: 76g
-# Todo: Money: $2.5
-# Todo: b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”. If
-# Todo: latte was their choice of drink.
+#  a. If the transaction is successful and there are enough resources to make the drink the
+#  user selected, then the ingredients to make the drink should be deducted from the
+#  coffee machine resources.
+#  E.g. report before purchasing latte:
+#  Water: 300ml
+#  Milk: 200ml
+#  Coffee: 100g
+#  Money: $0
+#  Report after purchasing latte:
+#  Water: 100ml
+#  Milk: 50ml
+#  Coffee: 76g
+#  Money: $2.5
+#  b. Once all resources have been deducted, tell the user “Here is your latte. Enjoy!”.
+#  If latte was their choice of drink.
+
