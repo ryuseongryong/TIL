016dd30dcea341aa7d6bbe655df93b5056bdf30c
ryuseongryong <ryuseongryong@gmail.com>
2022-02-25 23:59:13 +0900

    TDD-Node : mocking client - server

--- DreamCoding/TDD/UNIT-BASIC/src/mock/product/test/product_service_no_di.test.js
+++ DreamCoding/TDD/UNIT-BASIC/src/mock/product/test/product_service_no_di.test.js
@@ -1,2 +1,31 @@
 const ProductService = require('../product_service_no_di.js');
 const ProductClient = require('../product_client.js');
+jest.mock('../product_client');
+
+describe('ProductService', () => {
+  const fetchItems = jest.fn(async () => [
+    { item: 'Milk', available: true },
+    { item: 'Banana', available: false },
+  ]);
+  ProductClient.mockImplementation(() => {
+    return {
+      fetchItems,
+    };
+  });
+  let productService;
+
+  beforeEach(() => {
+    productService = new ProductService();
+  });
+
+  it('should filter out only available items', async () => {
+    const items = await productService.fetchAvailableItems();
+    expect(items).toEqual([{ item: 'Milk', available: true }]);
+    expect(items.length).toBe(1);
+  });
+
+  it('test', async () => {
+    const items = await productService.fetchAvailableItems();
+    expect(fetchItems).toHaveBeenCalledTimes(1);
+  });
+});
