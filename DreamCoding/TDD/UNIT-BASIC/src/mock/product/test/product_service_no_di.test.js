0918fa4ff4abc361c6ffdfe9695c4a6caf2c9b51
ryuseongryong <ryuseongryong@gmail.com>
2022-02-26 22:54:11 +0900

    TDD-Node : clearMocks setting

--- DreamCoding/TDD/UNIT-BASIC/src/mock/product/test/product_service_no_di.test.js
+++ DreamCoding/TDD/UNIT-BASIC/src/mock/product/test/product_service_no_di.test.js
@@ -16,11 +16,15 @@ describe('ProductService', () => {
 
   beforeEach(() => {
     productService = new ProductService();
+    // jest.config.js - clearMocks: false,
+    // fetchItems.mockClear()
+    // ProductClient.mockClear()
   });
 
   it('should filter out only available items', async () => {
     const items = await productService.fetchAvailableItems();
     expect(items).toEqual([{ item: 'Milk', available: true }]);
+    // jest.config.js - clearMocks: true,
     expect(items.length).toBe(1);
   });
 
