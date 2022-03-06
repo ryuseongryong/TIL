368427b54a2e2247f9258895acc0f3f6507446f2
ryuseongryong <ryuseongryong@gmail.com>
2022-03-06 14:12:46 +0900

    TDD : Dwitter-Server setting

---
+++ DreamCoding/TDD/dwitter-server/data/auth.js
@@ -0,0 +1,45 @@
+import SQ from 'sequelize';
+import { sequelize } from '../db/database.js';
+const DataTypes = SQ.DataTypes;
+
+export const User = sequelize.define(
+  'user',
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      allowNull: false,
+      primaryKey: true,
+    },
+    username: {
+      type: DataTypes.STRING(45),
+      allowNull: false,
+    },
+    password: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING(128),
+      allowNull: false,
+    },
+    url: DataTypes.TEXT,
+  },
+  { timestamps: false }
+);
+
+export async function findByUsername(username) {
+  return User.findOne({ where: { username } });
+}
+
+export async function findById(id) {
+  return User.findByPk(id);
+}
+
+export async function createUser(user) {
+  return User.create(user).then((data) => data.dataValues.id);
+}
---
+++ DreamCoding/TDD/dwitter-server/data/tweet.js
@@ -0,0 +1,81 @@
+import SQ from 'sequelize';
+import { sequelize } from '../db/database.js';
+import { User } from './auth.js';
+const DataTypes = SQ.DataTypes;
+const Sequelize = SQ.Sequelize;
+
+const Tweet = sequelize.define('tweet', {
+  id: {
+    type: DataTypes.INTEGER,
+    autoIncrement: true,
+    allowNull: false,
+    primaryKey: true,
+  },
+  text: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+});
+Tweet.belongsTo(User);
+
+const INCLUDE_USER = {
+  attributes: [
+    'id',
+    'text',
+    'createdAt',
+    'userId',
+    [Sequelize.col('user.name'), 'name'],
+    [Sequelize.col('user.username'), 'username'],
+    [Sequelize.col('user.url'), 'url'],
+  ],
+  include: {
+    model: User,
+    attributes: [],
+  },
+};
+
+const ORDER_DESC = {
+  order: [['createdAt', 'DESC']],
+};
+
+export async function getAll() {
+  return Tweet.findAll({ ...INCLUDE_USER, ...ORDER_DESC });
+}
+
+export async function getAllByUsername(username) {
+  return Tweet.findAll({
+    ...INCLUDE_USER,
+    ...ORDER_DESC,
+    include: {
+      ...INCLUDE_USER.include,
+      where: { username },
+    },
+  });
+}
+
+export async function getById(id) {
+  return Tweet.findOne({
+    where: { id },
+    ...INCLUDE_USER,
+  });
+}
+
+export async function create(text, userId) {
+  return Tweet.create({ text, userId }) //
+    .then((data) => this.getById(data.dataValues.id));
+}
+
+export async function update(id, text) {
+  return Tweet.findByPk(id, INCLUDE_USER) //
+    .then((tweet) => {
+      tweet.text = text;
+      return tweet.save();
+    });
+}
+
+export async function remove(id) {
+  return Tweet.findByPk(id) //
+    .then((tweet) => {
+      tweet.destroy();
+    });
+}
