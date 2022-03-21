df74c506d6793f3269899d27444788bc967cfacd
ryuseongryong <ryuseongryong@gmail.com>
2022-03-21 22:23:18 +0900

    TDD-feat : auth.test.js - unit test coverage : auth controller function me when no user

--- DreamCoding/TDD/dwitter-server/controller/test/auth.test.js
+++ DreamCoding/TDD/dwitter-server/controller/test/auth.test.js
@@ -1,3 +1,30 @@
-describe("", () => {
-  it("", () => {});
+import faker from "@faker-js/faker";
+import { AuthController } from "../auth.js";
+import httpMocks from "node-mocks-http";
+import { config } from "../../config.js";
+
+describe("Auth Controller", () => {
+  let authController, userRepository, userId, req, res;
+
+  beforeEach(() => {
+    userRepository = {};
+    authController = new AuthController(userRepository, config);
+    userId = faker.random.alphaNumeric(16);
+  });
+
+  it("returns 404 if user does not exist", async () => {
+    req = httpMocks.createRequest({
+      params: { id: userId },
+    });
+    res = httpMocks.createResponse();
+    userRepository.findById = jest.fn(() => null);
+
+    await authController.me(req, res);
+
+    expect(res.statusCode).toBe(404);
+    expect(res._getJSONData()).toMatchObject({
+      message: `User not found`,
+    });
+    expect(userRepository.findById).toHaveBeenCalledTimes(0);
+  });
 });
