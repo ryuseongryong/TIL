d548bd55fa2235ee963b44c0bcd48673e79a4737
ryuseongryong <ryuseongryong@gmail.com>
2022-05-30 00:01:12 +0900

    Fix day 17

--- Udemy/Python_Angela/pycharm ver/day17/quiz-game-start/main.py
+++ Udemy/Python_Angela/pycharm ver/day17/quiz-game-start/main.py
@@ -15,4 +15,4 @@ while quiz.still_has_questions():
     quiz.next_question()
 
 print("You've completed the quiz")
-f"Your final score was:"
+print(f"Your final score was: {quiz.score}/{quiz.question_number}")
