df4083b7c9ab4680031cabb1f7cc92f3bdb91238
ryuseongryong <ryuseongryong@gmail.com>
2022-06-01 23:22:06 +0900

    day 18-1 again

--- Udemy/Python_Angela/pycharm ver/day18/main.py
+++ Udemy/Python_Angela/pycharm ver/day18/main.py
@@ -1,6 +1,17 @@
 from turtle import Turtle, Screen
+import matplotlib
+
+matplotlib.use("TkAgg")
 
 timmy_the_turtle = Turtle()
+timmy_the_turtle.shape("turtle")
+timmy_the_turtle.color("red")
+timmy_the_turtle.forward(100)
+timmy_the_turtle.right(90)
+
+
+
+
 
 screen = Screen()
 screen.exitonclick()
