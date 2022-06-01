ab949c1a606b894079e373d253235fdccdc40dd3
ryuseongryong <ryuseongryong@gmail.com>
2022-06-01 23:52:36 +0900

    day 18-3 make dot square

--- Udemy/Python_Angela/pycharm ver/day18/main.py
+++ Udemy/Python_Angela/pycharm ver/day18/main.py
@@ -1,7 +1,4 @@
 from turtle import Turtle, Screen
-import matplotlib
-
-matplotlib.use("TkAgg")
 
 timmy_the_turtle = Turtle()
 # timmy_the_turtle.shape("turtle")
@@ -10,12 +7,11 @@ timmy_the_turtle = Turtle()
 # timmy_the_turtle.right(90)
 
 for _ in range(4):
-    timmy_the_turtle.forward(100)
     timmy_the_turtle.left(90)
-
-
-
-
+    for i in range(10):
+        timmy_the_turtle.dot()
+        timmy_the_turtle.penup()
+        timmy_the_turtle.forward(10)
 
 screen = Screen()
 screen.exitonclick()
