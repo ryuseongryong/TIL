40b63606021ca1619032048aae669070925574b5
ryuseongryong <ryuseongryong@gmail.com>
2022-06-02 00:00:25 +0900

    day 18-4 import module

--- Udemy/Python_Angela/pycharm ver/day18/main.py
+++ Udemy/Python_Angela/pycharm ver/day18/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import heroes
 
 timmy_the_turtle = Turtle()
 # timmy_the_turtle.shape("turtle")
@@ -6,12 +7,15 @@ timmy_the_turtle = Turtle()
 # timmy_the_turtle.forward(100)
 # timmy_the_turtle.right(90)
 
-for _ in range(4):
-    timmy_the_turtle.left(90)
-    for i in range(10):
-        timmy_the_turtle.dot()
-        timmy_the_turtle.penup()
-        timmy_the_turtle.forward(10)
+# for _ in range(4):
+#     timmy_the_turtle.left(90)
+#     for i in range(10):
+#         timmy_the_turtle.dot()
+#         timmy_the_turtle.penup()
+#         timmy_the_turtle.forward(10)
 
-screen = Screen()
-screen.exitonclick()
+# screen = Screen()
+# screen.exitonclick()
+
+
+print(heroes.gen())
