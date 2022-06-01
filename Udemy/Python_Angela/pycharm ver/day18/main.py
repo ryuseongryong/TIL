aae5fcbf9ac96359dd916b49a2d38de3273d6032
ryuseongryong <ryuseongryong@gmail.com>
2022-06-01 23:30:39 +0900

    day 18-2 make square

--- Udemy/Python_Angela/pycharm ver/day18/main.py
+++ Udemy/Python_Angela/pycharm ver/day18/main.py
@@ -4,10 +4,14 @@ import matplotlib
 matplotlib.use("TkAgg")
 
 timmy_the_turtle = Turtle()
-timmy_the_turtle.shape("turtle")
-timmy_the_turtle.color("red")
-timmy_the_turtle.forward(100)
-timmy_the_turtle.right(90)
+# timmy_the_turtle.shape("turtle")
+# timmy_the_turtle.color("red")
+# timmy_the_turtle.forward(100)
+# timmy_the_turtle.right(90)
+
+for _ in range(4):
+    timmy_the_turtle.forward(100)
+    timmy_the_turtle.left(90)
 
 
 
