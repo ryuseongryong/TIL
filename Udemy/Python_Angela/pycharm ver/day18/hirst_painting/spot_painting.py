3398fb7f062b74a62b9bf98d881328496e8e523c
ryuseongryong <ryuseongryong@gmail.com>
2022-06-05 23:35:11 +0900

    day 18-9 spot painting

---
+++ Udemy/Python_Angela/pycharm ver/day18/hirst_painting/spot_painting.py
@@ -0,0 +1,16 @@
+# import colorgram
+
+
+# rgb_colors = []
+# colors = colorgram.extract('/Users/seongryongryu/TIL/Udemy/Python_Angela/pycharm ver/day18/hirst_painting/image.jpg', 30)
+# # print(colors)
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+
+# print(rgb_colors)
+
+color_list = [(246, 241, 231), (226, 235, 242), (246, 234, 239), (233, 244, 238), (201, 161, 106), (146, 79, 62), (51, 101, 132), (135, 169, 187), (231, 213, 105), (192, 141, 158), (29, 40, 52), (138, 71, 86), (145, 176, 154), (162, 149, 65), (196, 95, 76), (71, 49, 37), (58, 114, 97), (33, 46, 39), (63, 149, 172), (219, 176, 187), (188, 96, 120), (103, 147, 110), (224, 178, 170), (27, 82, 74), (95, 47, 40), (45, 61, 98), (96, 47, 59), (22, 82, 91), (173, 202, 181), (178, 190, 209)]
