534ef72e52188961df2551a3769db03aa6a7df4d
ryuseongryong <ryuseongryong@gmail.com>
2022-06-08 23:37:45 +0900

    day 19-2 make turtle

--- Udemy/Python_Angela/pycharm ver/day19/main.py
+++ Udemy/Python_Angela/pycharm ver/day19/main.py
@@ -1,34 +1,45 @@
 from turtle import Turtle, Screen
 
-t = Turtle()
 screen = Screen()
+screen.setup(width=500, height=400)
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 
-def move_forwards():
-    t.forward(10)
-
-def move_backwards():
-    t.backward(10)
-
-def turn_left():
-    new_heading = t.heading() + 10
-    t.setheading(new_heading)
-
-def turn_right():
-    new_heading = t.heading() - 10
-    t.setheading(new_heading)
-
-def clear():
-    t.clear()
+for turtle_idx in range(0, len(colors)):
+    t = Turtle(shape="turtle")
+    t.color(colors[turtle_idx])
     t.penup()
-    t.home()
-    t.pendown()
+    t.goto(x=-230, y= -70 + turtle_idx * 30)
 
 
 
-screen.listen()
-screen.onkey(move_forwards, 'w')
-screen.onkey(move_backwards, 's')
-screen.onkey(turn_left, 'a')
-screen.onkey(turn_right, 'd')
-screen.onkey(clear, 'c')
 screen.exitonclick()
+
+# def move_forwards():
+#     t.forward(10)
+
+# def move_backwards():
+#     t.backward(10)
+
+# def turn_left():
+#     new_heading = t.heading() + 10
+#     t.setheading(new_heading)
+
+# def turn_right():
+#     new_heading = t.heading() - 10
+#     t.setheading(new_heading)
+
+# def clear():
+#     t.clear()
+#     t.penup()
+#     t.home()
+#     t.pendown()
+
+
+
+# screen.listen()
+# screen.onkey(move_forwards, 'w')
+# screen.onkey(move_backwards, 's')
+# screen.onkey(turn_left, 'a')
+# screen.onkey(turn_right, 'd')
+# screen.onkey(clear, 'c')
