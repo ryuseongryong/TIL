54c3486b6f66c31e3bc698bd72d9a124d99265b9
ryuseongryong <ryuseongryong@gmail.com>
2022-06-10 00:16:25 +0900

    day 19-3 turtle racing fin

--- Udemy/Python_Angela/pycharm ver/day19/main.py
+++ Udemy/Python_Angela/pycharm ver/day19/main.py
@@ -3,7 +3,7 @@ from turtle import Turtle, Screen
 
 screen = Screen()
 screen.setup(width=500, height=400)
-user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color: ")
+user_bet = screen.textinput(title="Make your bet", prompt="Which turtle will win the race? Enter a color(red/orange/yellow/green/blue/purple): ")
 colors = ["red", "orange", "yellow", "green", "blue", "purple"]
 in_race_on = False
 all_turtles = []
@@ -22,7 +22,12 @@ if user_bet:
 while is_race_on:
     for turtle in all_turtles:
         if turtle.xcor() > 230:
-            print(turtle.color())
+            is_race_on = False
+            winning_color = turtle.pencolor()
+            if winning_color == user_bet:
+                print(f"You've won! The {winning_color} turtle is the winner!")
+            else:
+                print(f"You've lose! The {winning_color} turtle is the winner!")
         rand_distance = random.randint(0, 10)
         turtle.forward(rand_distance)
 
