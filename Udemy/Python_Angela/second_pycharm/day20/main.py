648cf59b838266b64e88d09385fbffa45bf4b446
ryuseongryong <ryuseongryong@gmail.com>
2022-06-11 11:30:11 +0900

    day 20-1 snake game snake

--- Udemy/Python_Angela/second_pycharm/day20/main.py
+++ Udemy/Python_Angela/second_pycharm/day20/main.py
@@ -5,4 +5,18 @@ screen.setup(width=600, height=600)
 screen.bgcolor("black")
 screen.title("My Snake Game")
 
+starting_position = [(0, 0), (-20, 0), (-40, 0)]
+
+for position in starting_position:
+    new_segment = Turtle("square")
+    new_segment.color("white")
+    new_segment.goto(position)
+
+
+
+
+
+
+
+
 screen.exitonclick()
