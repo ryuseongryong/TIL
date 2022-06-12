7f890562290159093d740d32fc9cdf4aa32ca48d
ryuseongryong <ryuseongryong@gmail.com>
2022-06-12 23:04:37 +0900

    day 22 pong game paddle class

--- Udemy/Python_Angela/second_pycharm/day22/main.py
+++ Udemy/Python_Angela/second_pycharm/day22/main.py
@@ -1,4 +1,5 @@
 from turtle import Screen, Turtle
+from paddle import Paddle
 
 screen = Screen()
 screen.bgcolor("black")
@@ -6,26 +7,14 @@ screen.setup(width=800, height=600)
 screen.title("pong")
 screen.tracer(0)
 
-paddle = Turtle()
-paddle.shape("square")
-paddle.color("white")
-paddle.shapesize(stretch_wid=5, stretch_len=1)
-paddle.penup()
-paddle.goto(350, 0)
-
-def go_up():
-    if paddle.ycor() < 245:
-        new_y = paddle.ycor() + 20
-        paddle.goto(paddle.xcor(), new_y)
-
-def go_down():
-    if paddle.ycor() > -240:
-        new_y = paddle.ycor() - 20
-        paddle.goto(paddle.xcor(), new_y)
+r_paddle = Paddle((350, 0))
+l_paddle = Paddle((-350, 0))
 
 screen.listen()
-screen.onkey(go_up, "Up")
-screen.onkey(go_down, "Down")
+screen.onkey(r_paddle.go_up, "Up")
+screen.onkey(r_paddle.go_down, "Down")
+screen.onkey(l_paddle.go_up, "w")
+screen.onkey(l_paddle.go_down, "s")
 
 game_is_on = True
 while game_is_on:
