43c1269cc58b8667b44c3dd1d0d44208329eeb22
ryuseongryong <ryuseongryong@gmail.com>
2022-06-12 22:46:14 +0900

    day 22 pong game paddle

---
+++ Udemy/Python_Angela/second_pycharm/day22/main.py
@@ -0,0 +1,34 @@
+from turtle import Screen, Turtle
+
+screen = Screen()
+screen.bgcolor("black")
+screen.setup(width=800, height=600)
+screen.title("pong")
+screen.tracer(0)
+
+paddle = Turtle()
+paddle.shape("square")
+paddle.color("white")
+paddle.shapesize(stretch_wid=5, stretch_len=1)
+paddle.penup()
+paddle.goto(350, 0)
+
+def go_up():
+    if paddle.ycor() < 245:
+        new_y = paddle.ycor() + 20
+        paddle.goto(paddle.xcor(), new_y)
+
+def go_down():
+    if paddle.ycor() > -240:
+        new_y = paddle.ycor() - 20
+        paddle.goto(paddle.xcor(), new_y)
+
+screen.listen()
+screen.onkey(go_up, "Up")
+screen.onkey(go_down, "Down")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+
+screen.exitonclick()
