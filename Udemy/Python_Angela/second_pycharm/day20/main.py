87f7251e55a80ac460280362ef3a39239da6f60f
ryuseongryong <ryuseongryong@gmail.com>
2022-06-12 14:40:47 +0900

    day 20 snake game use slice

--- Udemy/Python_Angela/second_pycharm/day20/main.py
+++ Udemy/Python_Angela/second_pycharm/day20/main.py
@@ -35,10 +35,8 @@ while game_is_on:
         game_is_on = False
         scoreboard.game_over()
 
-    for segment in snake.segments:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 15:
+    for segment in snake.segments[1:]:
+        if snake.head.distance(segment) < 15:
             game_is_on = False
             scoreboard.game_over()
 
