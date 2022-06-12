5c46bf62e20bd356d458099934ab34353ab713c3
ryuseongryong <ryuseongryong@gmail.com>
2022-06-12 13:55:03 +0900

    day 20 snake game crash wall

--- Udemy/Python_Angela/second_pycharm/day20/main.py
+++ Udemy/Python_Angela/second_pycharm/day20/main.py
@@ -30,5 +30,9 @@ while game_is_on:
         food.refresh()
         scoreboard.increase_score()
 
+    if snake.head.xcor() > 280 or snake.head.xcor() < -280 or snake.head.ycor() > 280 or snake.head.ycor() < -280:
+        game_is_on = False
+        scoreboard.game_over()
+
 
 screen.exitonclick()
--- Udemy/Python_Angela/second_pycharm/day20/scoreboard.py
+++ Udemy/Python_Angela/second_pycharm/day20/scoreboard.py
@@ -16,9 +16,12 @@ class scoreboard(Turtle):
 
     def update_scoreboard(self):
         self.write(f"Score: {self.score}", align=ALIGNMENT, font=FONT)
-        
 
     def increase_score(self):
         self.score += 1
         self.clear()
         self.update_scoreboard()
+
+    def game_over(self):
+        self.goto(0, 0)
+        self.write(f"Game Over", align=ALIGNMENT, font=FONT)
