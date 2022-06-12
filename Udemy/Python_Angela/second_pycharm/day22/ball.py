7a5ad9934e335d4a4b077ea512635795def080e7
ryuseongryong <ryuseongryong@gmail.com>
2022-06-12 23:46:24 +0900

    day 22 pong game bounce2

--- Udemy/Python_Angela/second_pycharm/day22/ball.py
+++ Udemy/Python_Angela/second_pycharm/day22/ball.py
@@ -16,5 +16,7 @@ class Ball(Turtle):
         new_y = self.ycor() + self.y_move
         self.goto(new_x, new_y)
 
-    def bounce(self):
+    def bounce_x(self):
+        self.x_move *= -1
+    def bounce_y(self):
         self.y_move *= -1
