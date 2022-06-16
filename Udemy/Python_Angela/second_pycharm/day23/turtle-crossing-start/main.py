5327bcc03d59df35add3711b7764d9ef00ae9b90
ryuseongryong <ryuseongryong@gmail.com>
2022-06-17 00:02:26 +0900

    day 23 move the cars

--- Udemy/Python_Angela/second_pycharm/day23/turtle-crossing-start/main.py
+++ Udemy/Python_Angela/second_pycharm/day23/turtle-crossing-start/main.py
@@ -2,7 +2,7 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
-from scoreboard import Scoreboard
+# from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
