a1d5366e7bd00f873117cbdcc4c4f7059503fa06
ryuseongryong <ryuseongryong@gmail.com>
2022-06-18 23:58:28 +0900

    day 23 turtle crossing done

--- Udemy/Python_Angela/second_pycharm/day23/turtle-crossing-start/main.py
+++ Udemy/Python_Angela/second_pycharm/day23/turtle-crossing-start/main.py
@@ -2,7 +2,7 @@ import time
 from turtle import Screen
 from player import Player
 from car_manager import CarManager
-# from scoreboard import Scoreboard
+from scoreboard import Scoreboard
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -10,6 +10,7 @@ screen.tracer(0)
 
 player = Player()
 car_manager = CarManager()
+scoreboard = Scoreboard()
 
 screen.listen()
 screen.onkey(player.go_up, "Up")
@@ -25,9 +26,11 @@ while game_is_on:
     for car in car_manager.all_cars:
         if car.distance(player) < 20:
             game_is_on = False
+            scoreboard.game_over()
 
     if player.is_at_finish_line():
         player.go_to_start_line()
+        scoreboard.increase_level()
         car_manager.level_up()
 
 
