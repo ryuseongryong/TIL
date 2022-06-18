b4712cac446ddf77f41712f2a23fbb0a5e128690
ryuseongryong <ryuseongryong@gmail.com>
2022-06-18 23:43:32 +0900

    day 23 detect collision

--- Udemy/Python_Angela/second_pycharm/day23/turtle-crossing-start/main.py
+++ Udemy/Python_Angela/second_pycharm/day23/turtle-crossing-start/main.py
@@ -21,3 +21,9 @@ while game_is_on:
 
     car_manager.create_car()
     car_manager.move_cars()
+
+    for car in car_manager.all_cars:
+        if car.distance(player) < 20:
+            game_is_on = False
+
+screen.exitonclick()
