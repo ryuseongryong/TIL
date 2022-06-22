1223e9d26a7ac6d3c647f0ff8ef685d3d67ed314
ryuseongryong <ryuseongryong@gmail.com>
2022-06-22 23:56:45 +0900

    day 24 mail merge completed

--- Udemy/Python_Angela/second_pycharm/day24/mail_merge/Mail Merge Project Start/main.py
+++ Udemy/Python_Angela/second_pycharm/day24/mail_merge/Mail Merge Project Start/main.py
@@ -1,8 +1,28 @@
-#TODO: Create a letter using starting_letter.txt 
-#for each name in invited_names.txt
-#Replace the [name] placeholder with the actual name.
-#Save the letters in the folder "ReadyToSend".
-    
-#Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
-    #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
-        #Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+# TODO: Create a letter using starting_letter.txt
+# for each name in invited_names.txt
+# Replace the [name] placeholder with the actual name.
+# Save the letters in the folder "ReadyToSend".
+
+# Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
+# Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
+# Hint3: THis method will help you: https://www.w3schools.com/python/ref_string_strip.asp
+import os
+
+os.chdir(
+    "./Udemy/Python_Angela/second_pycharm/day24/mail_merge/Mail Merge Project Start/"
+)
+
+PLACEHOLDER = "[name]"
+
+with open("./Input/Names/invited_names.txt", "r") as names_file:
+    names = names_file.readlines()
+
+with open("./Input/Letters/starting_letter.txt", "r") as letter_file:
+    letters = letter_file.read()
+    for name in names:
+        stripped_name = name.strip()
+        new_letter = letters.replace(PLACEHOLDER, stripped_name)
+        with open(
+            f"./Output/ReadyToSend/letter_for_{stripped_name}.txt", "w"
+        ) as completed_letter:
+            completed_letter.write(new_letter)
