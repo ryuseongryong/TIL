010fe1d049d92991c8f44788a6be013b48d12e9b
ryuseongryong <ryuseongryong@gmail.com>
2022-06-25 23:51:30 +0900

    day 25 pandas more

--- Udemy/Python_Angela/second_local/day25/main.py
+++ Udemy/Python_Angela/second_local/day25/main.py
@@ -22,4 +22,38 @@ os.chdir("./Udemy/Python_Angela/second_local/day25")
 
 
 data = pandas.read_csv("weather_data.csv")
-print(data["temp"])
+# print(type(data))
+# print(type(data["temp"]))
+
+data_dict = data.to_dict()
+# print(data_dict)
+
+temp_list = data["temp"].to_list()
+# print(temp_list)
+
+# temp_avg = sum(temp_list) / len(temp_list)
+# print(temp_avg)
+# print(data["temp"].mean())
+
+temp_max = data["temp"].max()
+# print(temp_max)
+
+# Get Data in Columns
+# print(data["condition"])
+# print(data.condition)
+
+# Get Data in Row
+monday = data[data.day == "Monday"]
+# print(row_monday)
+
+row_max_temp = data[data.temp == temp_max]
+# print(row_max_temp)
+
+monday_temp_cel = int(monday.temp)
+monday_temp_fah = (monday_temp_cel * 9 / 5) + 32
+print(monday_temp_cel, monday_temp_fah)
+
+# Create a DataFrame from Scratch
+data_dict = {"students": ["Amy", "James", "Angela"], "scores": [76, 56, 65]}
+data = pandas.DataFrame(data_dict)
+data.to_csv("new_data.csv")
