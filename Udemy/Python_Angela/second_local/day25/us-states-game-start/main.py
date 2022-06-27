33f53dc688c801cb3fbdf5b69b35edc628f8a7fc
ryuseongryong <ryuseongryong@gmail.com>
2022-06-27 23:59:30 +0900

    day 25 U.S. states quiz answer

--- Udemy/Python_Angela/second_local/day25/us-states-game-start/main.py
+++ Udemy/Python_Angela/second_local/day25/us-states-game-start/main.py
@@ -1,6 +1,7 @@
 import turtle, os
+import pandas
 
-print(os.getcwd())
+# print(os.getcwd())
 os.chdir("./Udemy/Python_Angela/second_local/day25/us-states-game-start")
 
 screen = turtle.Screen()
@@ -8,9 +9,24 @@ screen.title("U.S. States Game")
 screen.addshape("blank_states_img.gif")
 turtle.shape("blank_states_img.gif")
 
-answer_state = screen.textinput(
-    title="Guess the State", prompt="What is another state's name?"
-)
-print(answer_state)
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(
+        title="Guess the State", prompt="What is another state's name?"
+    )
+    print(answer_state)
+
+    if answer_state in all_states:
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        # t.write(answer_state)
+        t.write(state_data.state.item())
 
 screen.exitonclick()
