289b83bdd7537cf5943db4c89e7c429fee33bbdc
ryuseongryong <ryuseongryong@gmail.com>
2022-06-29 00:06:22 +0900

    Fix day 25 US States quiz

--- Udemy/Python_Angela/second_local/day25/us-states-game-start/main.py
+++ Udemy/Python_Angela/second_local/day25/us-states-game-start/main.py
@@ -19,17 +19,19 @@ while len(guessed_states) < 50:
         title=f"{len(guessed_states)}/50 States Correct",
         prompt="What is another state's name?",
     ).title()
-    print(answer_state)
 
     if answer_state == "Exit":
         missing_states = []
         for state in all_states:
             if state not in guessed_states:
                 missing_states.append(state)
+        print(missing_states)
+        print(guessed_states)
         new_data = pandas.DataFrame(missing_states)
         new_data.to_csv("states_to_learn.csv")
         break
     if answer_state in all_states:
+        guessed_states.append(answer_state)
         t = turtle.Turtle()
         t.hideturtle()
         t.penup()
