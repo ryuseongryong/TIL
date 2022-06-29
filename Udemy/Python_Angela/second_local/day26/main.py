40d359faf08140205026f88b110a3df59c3c024f
ryuseongryong <ryuseongryong@gmail.com>
2022-06-30 00:22:23 +0900

    day 26 list comprehension

--- Udemy/Python_Angela/second_local/day26/main.py
+++ Udemy/Python_Angela/second_local/day26/main.py
@@ -1,5 +1,21 @@
 number = [1, 2, 3]
 new = [n + 1 for n in number]
 
-print(number)
-print(new)
+name = "ryuseongryong"
+letters = [letter for letter in name]
+
+# print(number)
+# print(new)
+print(letters)
+
+range_num = [n * 2 for n in range(1, 5)]
+print(range_num)
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+short_names = [name for name in names if len(name) < 5]
+
+print(names)
+print(short_names)
+
+cap_names = [name.upper() for name in names if len(name) > 4]
+print(cap_names)
