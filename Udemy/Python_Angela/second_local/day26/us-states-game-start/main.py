b03090ad7ecf130403824885ea9f418c3b490f5c
ryuseongryong <ryuseongryong@gmail.com>
2022-07-01 23:57:08 +0900

    day 26 list comprehension for U.S. states game

---
+++ Udemy/Python_Angela/second_local/day26/us-states-game-start/main.py
@@ -0,0 +1,41 @@
+import turtle, os
+import pandas
+
+# print(os.getcwd())
+os.chdir("./Udemy/Python_Angela/second_local/day25/us-states-game-start")
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.addshape("blank_states_img.gif")
+turtle.shape("blank_states_img.gif")
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+
+guessed_states = []
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(
+        title=f"{len(guessed_states)}/50 States Correct",
+        prompt="What is another state's name?",
+    ).title()
+
+    if answer_state == "Exit":
+        missing_states = [state for state in all_states if state not in guessed_states]
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        # t.write(answer_state)
+        t.write(state_data.state.item())
+
+# states_to_learn.csv
+
+
+# screen.exitonclick()
