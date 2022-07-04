109259b2813ed393c23b9f880750a4f30b31287d
ryuseongryong <ryuseongryong@gmail.com>
2022-07-04 23:58:55 +0900

    day 26 pandas df with dict and phonetic dict and list

---
+++ Udemy/Python_Angela/second_local/day26/NATO-alphabet-start/main.py
@@ -0,0 +1,34 @@
+import os
+
+os.chdir("./Udemy/Python_Angela/second_local/day26/NATO-alphabet-start")
+student_dict = {"student": ["Angela", "James", "Lily"], "score": [56, 76, 98]}
+
+# Looping through dictionaries:
+for (key, value) in student_dict.items():
+    # Access key and value
+    pass
+
+import pandas
+
+student_data_frame = pandas.DataFrame(student_dict)
+
+# Loop through rows of a data frame
+for (index, row) in student_data_frame.iterrows():
+    # Access index and row
+    # Access row.student or row.score
+    pass
+
+# Keyword Method with iterrows()
+# {new_key:new_value for (index, row) in df.iterrows()}
+
+# TODO 1. Create a dictionary in this format:
+{"A": "Alfa", "B": "Bravo"}
+phonetic_csv = pandas.read_csv("nato_phonetic_alphabet.csv")
+print(phonetic_csv.to_dict())
+phonetic_dict = {row.letter: row.code for (index, row) in phonetic_csv.iterrows()}
+print(phonetic_dict)
+
+# TODO 2. Create a list of the phonetic code words from a word that the user inputs.
+word = input("Enter a word: ").upper()
+output_list = [phonetic_dict[letter] for letter in word]
+print(output_list)
--- Udemy/Python_Angela/second_local/day26/main.py
+++ Udemy/Python_Angela/second_local/day26/main.py
@@ -1,67 +1,23 @@
-import os
+student_dict = {"student": ["Angela", "James", "Lily"], "score": [56, 76, 98]}
 
-os.chdir("./Udemy/Python_Angela/second_local/day26")
-# first
-# number = [1, 2, 3]
-# new = [n + 1 for n in number]
+# Looping through dictionaries
+for (key, value) in student_dict.items():
+    print(key)
+    print(value)
 
-# name = "ryuseongryong"
-# letters = [letter for letter in name]
+import pandas
 
-# # print(number)
-# # print(new)
-# print(letters)
+student_df = pandas.DataFrame(student_dict)
+print(student_df)
 
-# range_num = [n * 2 for n in range(1, 5)]
-# print(range_num)
+# Loop through a data frame
+# for (key, value) in student_df.items():
+#     print(key)
+#     print(value)
 
-# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
-# short_names = [name for name in names if len(name) < 5]
-
-# print(names)
-# print(short_names)
-
-# cap_names = [name.upper() for name in names if len(name) > 4]
-# print(cap_names)
-
-# second
-# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-
-# squared_numbers = [n**2 for n in numbers]
-
-# print(squared_numbers)
-
-# third
-# numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
-
-# result = [n for n in numbers if n % 2 == 0]
-
-# print(result)
-
-# forth
-with open("file1.txt") as f1:
-    nums = f1.read()
-    nums = nums.splitlines()
-    nums = [int(n) for n in nums]
-    nums.sort()
-    f1_nums = nums
-with open("file2.txt") as f2:
-    nums = f2.read()
-    nums = nums.splitlines()
-    nums = [int(n) for n in nums]
-    nums.sort()
-    f2_nums = nums
-
-print(f1_nums)
-print(f2_nums)
-
-result = [n for n in f1_nums if n in f2_nums]
-print(result)
-# ---
-# forth-ref
-with open("file1.txt") as file1:
-    file_1_data = file1.readlines()
-with open("file2.txt") as file2:
-    file_2_data = file2.readlines()
-
-result = [int(num) for num in file_1_data if num in file_2_data]
+# Loop through rows of a data frame
+for (index, row) in student_df.iterrows():
+    # print(index)
+    # print(row.student)
+    if row.student == "Lily":
+        print(row.score)
