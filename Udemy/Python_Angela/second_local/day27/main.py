770489128cbcc34632ec4fbaa1b581908eb240e6
ryuseongryong <ryuseongryong@gmail.com>
2022-07-05 23:50:31 +0900

    day 27 tkinter

---
+++ Udemy/Python_Angela/second_local/day27/main.py
@@ -0,0 +1,21 @@
+import tkinter
+
+window = tkinter.Tk()
+window.title("My First GUI Program")
+window.minsize(width=500, height=300)
+
+# Label
+
+my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "italic"))
+# my_label.pack(side="left")
+my_label.pack(expand=True)
+
+
+import turtle
+
+t = turtle.Turtle()
+t.write()
+
+window.mainloop()
+# https://docs.python.org/3/library/tkinter.html
+# https://tcl.tk/man/tcl8.6/TkCmd/pack.htm
