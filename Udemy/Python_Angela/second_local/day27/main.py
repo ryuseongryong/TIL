c7136bceaefb82b545608a915afe179b1e09f6d6
ryuseongryong <ryuseongryong@gmail.com>
2022-07-06 23:54:47 +0900

    day 27 *args unlimited positional arguments

--- Udemy/Python_Angela/second_local/day27/main.py
+++ Udemy/Python_Angela/second_local/day27/main.py
@@ -1,21 +1,40 @@
-import tkinter
+# import tkinter
 
-window = tkinter.Tk()
-window.title("My First GUI Program")
-window.minsize(width=500, height=300)
+# window = tkinter.Tk()
+# window.title("My First GUI Program")
+# window.minsize(width=500, height=300)
 
-# Label
+# # Label
 
-my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "italic"))
-# my_label.pack(side="left")
-my_label.pack(expand=True)
+# my_label = tkinter.Label(text="I Am a Label", font=("Arial", 24, "italic"))
+# # my_label.pack(side="left")
+# my_label.pack(expand=True)
 
 
-import turtle
+# import turtle
 
-t = turtle.Turtle()
-t.write()
+# t = turtle.Turtle()
+# t.write("hello")
 
-window.mainloop()
-# https://docs.python.org/3/library/tkinter.html
-# https://tcl.tk/man/tcl8.6/TkCmd/pack.htm
+# window.mainloop()
+# # https://docs.python.org/3/library/tkinter.html
+# # https://tcl.tk/man/tcl8.6/TkCmd/pack.htm
+
+# Default value Arguments
+# def foo(a, b=2, c=3):
+#     print(a, b, c)
+
+
+# Unlimited Arguments
+def add(*args):
+    # type of args = tuple
+    print(args)
+    print(args[0])
+
+    sum = 0
+    for n in args:
+        sum += n
+    return sum
+
+
+print(add(1, 2, 3))
