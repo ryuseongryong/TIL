9f87d3d01cfb32fd3ca2fa1d5136fc45f69eafd7
ryuseongryong <ryuseongryong@gmail.com>
2022-07-10 20:52:58 +0900

    day 27 tkinter grid and padding

--- Udemy/Python_Angela/second_local/day27/main.py
+++ Udemy/Python_Angela/second_local/day27/main.py
@@ -3,6 +3,7 @@ from tkinter import *
 window = Tk()
 window.title("My First GUI Program")
 window.minsize(width=500, height=300)
+window.config(padx=110, pady=220)
 
 # Label
 
@@ -13,6 +14,9 @@ my_label.pack()
 
 my_label["text"] = "New Text"
 my_label.config(text="New Text")
+# my_label.place(x=0, y=0)
+my_label.grid(column=0, row=0)
+my_label.config(padx=50, pady=50)
 
 
 def button_clicked():
@@ -21,13 +25,17 @@ def button_clicked():
 
 
 button = Button(text="Click Me", command=button_clicked)
-button.pack()
+# button.pack()
+button.grid(column=1, row=1)
+
+new_button = Button(text="New Button")
+new_button.grid(column=2, row=0)
 
 # Entry
 input = Entry(width=10)
-input.pack()
+# input.pack()
 print(input.get())
-
+input.grid(column=3, row=2)
 
 window.mainloop()
 # https://docs.python.org/3/library/tkinter.html
