03172ca4d09c8584289f483a18ff2f72348bc59e
ryuseongryong <ryuseongryong@gmail.com>
2022-07-11 23:59:25 +0900

    day 27 converter layout

---
+++ Udemy/Python_Angela/second_local/day27/mile_km_converter.py
@@ -0,0 +1,29 @@
+from curses import window
+from tkinter import *
+
+window = Tk()
+window.title("Miles to Kilometer Converter")
+window.config(padx=20, pady=20)
+
+miles_input = Entry(width=7)
+miles_input.grid(column=1, row=0)
+
+miles_label = Label(text="Miles")
+miles_label.grid(column=2, row=0)
+
+
+is_equal_label = Label(text="is equal to")
+is_equal_label.grid(column=0, row=1)
+
+kilometer_result_label = Label(text="0")
+kilometer_result_label.grid(column=1, row=1)
+
+kilometer_label = Label(text="Km")
+kilometer_label.grid(column=2, row=1)
+
+
+calculate_button = Button(text="Calculate")
+calculate_button.grid(column=1, row=2)
+
+
+window.mainloop()
