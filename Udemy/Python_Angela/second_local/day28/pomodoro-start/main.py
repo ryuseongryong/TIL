954b8e378c913b4533d51705315ab78f6f5f81e4
ryuseongryong <ryuseongryong@gmail.com>
2022-07-14 02:13:24 +0900

    day 28 pomodoro layout

--- Udemy/Python_Angela/second_local/day28/pomodoro-start/main.py
+++ Udemy/Python_Angela/second_local/day28/pomodoro-start/main.py
@@ -26,12 +26,27 @@ LONG_BREAK_MIN = 20
 
 window = Tk()
 window.title("Pomodoro")
-window.config(padx=20, pady=10, bg=YELLOW)
+window.config(padx=20, pady=20, bg=YELLOW)
+
+title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+title_label.grid(column=1, row=0)
 
 canvas = Canvas(width=413, height=531, bg=YELLOW, highlightthickness=0)
 bg_img = PhotoImage(file="image.png")
 canvas.create_image(206, 265, image=bg_img)
 canvas.create_text(206, 385, text="00:00", fill="black", font=(FONT_NAME, 35, "bold"))
-canvas.pack()
+canvas.grid(column=1, row=1)
+
+start_button = Button(
+    text="Start", bg=YELLOW, highlightthickness=0, highlightbackground=YELLOW
+)
+start_button.grid(column=0, row=2)
+reset_button = Button(
+    text="Reset", bg=YELLOW, highlightthickness=0, highlightbackground=YELLOW
+)
+reset_button.grid(column=2, row=2)
+
+check_marker = Label(text="✓", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
+check_marker.grid(column=1, row=2)
 
 window.mainloop()
