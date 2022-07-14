293049d0ffdbe2e4658b870503348ac6eebcd272
ryuseongryong <ryuseongryong@gmail.com>
2022-07-14 23:56:38 +0900

    day 28 pomodoro timer default

--- Udemy/Python_Angela/second_local/day28/pomodoro-start/main.py
+++ Udemy/Python_Angela/second_local/day28/pomodoro-start/main.py
@@ -1,8 +1,8 @@
 from tkinter import *
 
-# import os
+import os
 
-# os.chdir("./Udemy/Python_Angela/second_local/day28/pomodoro-start/")
+os.chdir("./Udemy/Python_Angela/second_local/day28/pomodoro-start/")
 # print(os.getcwd())
 
 # ---------------------------- CONSTANTS ------------------------------- #
@@ -18,8 +18,20 @@ LONG_BREAK_MIN = 20
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
+def start_timer():
+    count_down(5 * 60)
+
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
+def count_down(count):
+
+    count_min = count // 60
+    count_sec = count % 60
+
+    canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
+    if count > 0:
+        window.after(1000, count_down, count - 1)
+
 
 # ---------------------------- UI SETUP ------------------------------- #
 
@@ -28,17 +40,25 @@ window = Tk()
 window.title("Pomodoro")
 window.config(padx=20, pady=20, bg=YELLOW)
 
+
 title_label = Label(text="Timer", fg=GREEN, bg=YELLOW, font=(FONT_NAME, 50))
 title_label.grid(column=1, row=0)
 
 canvas = Canvas(width=413, height=531, bg=YELLOW, highlightthickness=0)
 bg_img = PhotoImage(file="image.png")
 canvas.create_image(206, 265, image=bg_img)
-canvas.create_text(206, 385, text="00:00", fill="black", font=(FONT_NAME, 35, "bold"))
+timer_text = canvas.create_text(
+    206, 385, text="00:00", fill="black", font=(FONT_NAME, 35, "bold")
+)
 canvas.grid(column=1, row=1)
 
+
 start_button = Button(
-    text="Start", bg=YELLOW, highlightthickness=0, highlightbackground=YELLOW
+    text="Start",
+    bg=YELLOW,
+    highlightthickness=0,
+    highlightbackground=YELLOW,
+    command=start_timer,
 )
 start_button.grid(column=0, row=2)
 reset_button = Button(
