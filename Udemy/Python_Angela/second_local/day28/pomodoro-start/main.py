a1d6e2d175de34e89914f47f26ac297719a3e957
ryuseongryong <ryuseongryong@gmail.com>
2022-07-16 14:06:09 +0900

    day 28 pomodoro timer working set, breaking set and title label

--- Udemy/Python_Angela/second_local/day28/pomodoro-start/main.py
+++ Udemy/Python_Angela/second_local/day28/pomodoro-start/main.py
@@ -14,12 +14,27 @@ FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
+reps = 0
 
 # ---------------------------- TIMER RESET ------------------------------- #
 
 # ---------------------------- TIMER MECHANISM ------------------------------- #
 def start_timer():
-    count_down(5 * 60)
+    global reps
+    reps += 1
+    work_sec = WORK_MIN * 60
+    short_break_sec = SHORT_BREAK_MIN * 60
+    long_break_sec = LONG_BREAK_MIN * 60
+
+    if reps % 8 == 0:
+        count_down(long_break_sec)
+        title_label.config(text="Break", fg=RED, bg=YELLOW)
+    elif reps % 2 == 0:
+        count_down(short_break_sec)
+        title_label.config(text="Break", fg=PINK, bg=YELLOW)
+    else:
+        count_down(work_sec)
+        title_label.config(text="Work", fg=GREEN, bg=YELLOW)
 
 
 # ---------------------------- COUNTDOWN MECHANISM ------------------------------- #
@@ -35,6 +50,8 @@ def count_down(count):
     canvas.itemconfig(timer_text, text=f"{count_min}:{count_sec}")
     if count > 0:
         window.after(1000, count_down, count - 1)
+    else:
+        start_timer()
 
 
 # ---------------------------- UI SETUP ------------------------------- #
