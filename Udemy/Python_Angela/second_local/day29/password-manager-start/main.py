30e95177c18941f75525a52247621ea3709afe9b
ryuseongryong <ryuseongryong@gmail.com>
2022-07-19 21:49:33 +0900

    day 29 Fix password manager layout

--- Udemy/Python_Angela/second_local/day29/password-manager-start/main.py
+++ Udemy/Python_Angela/second_local/day29/password-manager-start/main.py
@@ -10,10 +10,31 @@ os.chdir("./Udemy/Python_Angela/second_local/day29/password-manager-start")
 
 window = Tk()
 window.title("Password Manager")
+window.config(padx=50, pady=50)
 
 canvas = Canvas(height=200, width=200)
 logo_img = PhotoImage(file="logo.png")
 canvas.create_image(100, 100, image=logo_img)
-canvas.pack()
+canvas.grid(row=0, column=1)
+
+
+website_label = Label(text="Website")
+website_label.grid(row=1, column=0)
+email_label = Label(text="Email/Username")
+email_label.grid(row=2, column=0)
+password_label = Label(text="password")
+password_label.grid(row=3, column=0)
+
+website_entry = Entry(width=39)
+website_entry.grid(row=1, column=1, columnspan=2)
+email_entry = Entry(width=39)
+email_entry.grid(row=2, column=1, columnspan=2)
+password_entry = Entry(width=21)
+password_entry.grid(row=3, column=1)
+
+generate_password_button = Button(text="Generate Password")
+generate_password_button.grid(row=3, column=2)
+add_button = Button(text="Add", width=36)
+add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
