b1e85818438408e4e71a5a964d2a5f28f5ade89b
ryuseongryong <ryuseongryong@gmail.com>
2022-07-20 23:59:35 +0900

    day 29 Fix password manager func

--- Udemy/Python_Angela/second_local/day29/password-manager-start/main.py
+++ Udemy/Python_Angela/second_local/day29/password-manager-start/main.py
@@ -6,6 +6,19 @@ os.chdir("./Udemy/Python_Angela/second_local/day29/password-manager-start")
 
 # ---------------------------- SAVE PASSWORD ------------------------------- #
 
+
+def save():
+
+    website = website_entry.get()
+    email = email_entry.get()
+    password = password_entry.get()
+
+    with open("data.txt", "a") as data_file:
+        data_file.write(f"{website} | {email} | {password}")
+        website_entry.delete(0, END)
+        password_entry.delete(0, END)
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -27,14 +40,16 @@ password_label.grid(row=3, column=0)
 
 website_entry = Entry(width=39)
 website_entry.grid(row=1, column=1, columnspan=2)
+website_entry.focus()
 email_entry = Entry(width=39)
 email_entry.grid(row=2, column=1, columnspan=2)
+email_entry.insert(0, "ryuseongryong@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
 generate_password_button = Button(text="Generate Password")
 generate_password_button.grid(row=3, column=2)
-add_button = Button(text="Add", width=36)
+add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
 
 window.mainloop()
