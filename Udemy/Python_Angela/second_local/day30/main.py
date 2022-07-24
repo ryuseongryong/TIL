441510b8ad308c4012fb333144ad446b8fea0c0a
ryuseongryong <ryuseongryong@gmail.com>
2022-07-24 14:51:48 +0900

    day 30 self except raise

--- Udemy/Python_Angela/second_local/day30/main.py
+++ Udemy/Python_Angela/second_local/day30/main.py
@@ -4,22 +4,6 @@ os.chdir("./Udemy/Python_Angela/second_local/day30")
 # FileNotFound
 # with open("file.txt") as file:
 #     file.read()
-try:
-    file = open("file.txt")
-    d = {"key": "val"}
-    print(d["non_exist_key"])
-    print(d["key"])
-
-except FileNotFoundError:
-    file = open("file.txt", "w")
-    file.write("blah blah")
-except KeyError as error_message:
-    print(f"That key {error_message} does not exist")
-else:
-    content = file.read()
-    print(content)
-finally:
-    file.close()
 # KeyError
 # d = {"key": "val"}
 # v = d["non_exist_key"]
@@ -31,3 +15,30 @@ finally:
 # TypeError
 # text = "abc"
 # print(text + 5)
+
+# try:
+#     file = open("file.txt")
+#     d = {"key": "val"}
+#     print(d["non_exist_key"])
+#     print(d["key"])
+
+# except FileNotFoundError:
+#     file = open("file.txt", "w")
+#     file.write("blah blah")
+# except KeyError as error_message:
+#     print(f"That key {error_message} does not exist")
+# else:
+#     content = file.read()
+#     print(content)
+# finally:
+#     file.close()
+#     print("File was closed")
+
+height = float(input("Height: "))
+weight = int(input("Weight: "))
+
+if height > 3:
+    raise ValueError("Human Height should not be over 3 meters")
+
+bmi = weight / height**2
+print(bmi)
