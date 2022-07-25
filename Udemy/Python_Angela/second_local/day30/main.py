fa937b979305a061217d2366bb8ef6b3db4be2de
ryuseongryong <ryuseongryong@gmail.com>
2022-07-25 21:56:38 +0900

    day 30 KeyError Handling

--- Udemy/Python_Angela/second_local/day30/main.py
+++ Udemy/Python_Angela/second_local/day30/main.py
@@ -44,13 +44,32 @@ os.chdir("./Udemy/Python_Angela/second_local/day30")
 # print(bmi)
 
 # IndexError Handling
-fruits = ["Apple", "Pear", "Orange"]
+# fruits = ["Apple", "Pear", "Orange"]
 
 
-def make_pie(index):
+# def make_pie(index):
+#     try:
+#         fruit = fruits[index]
+#     except IndexError:
+#         print("Fruit pie")
+#     else:
+#         print(fruit + " pie")
+
+
+# KeyError Handling
+facebook_posts = [
+    {"Likes": 1, "Comments": 2},
+    {"Likes": 5, "Comments": 3},
+    {"Likes": 8, "Comments": 9, "Shares": 1},
+    {"Likes": 9, "Comments": 2, "Shares": 3},
+    {"Comments": 5, "Shares": 1},
+    {"Comments": 1, "Shares": 6},
+]
+total_likes = 0
+for post in facebook_posts:
     try:
-        fruit = fruits[index]
-    except IndexError:
-        print("Fruit pie")
-    else:
-        print(fruit + " pie")
+        total_likes = total_likes + post["likes"]
+    except KeyError:
+        pass
+
+print(total_likes)
