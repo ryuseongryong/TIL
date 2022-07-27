d9a0d45aaf6bd6a76c55da7ca9c9f141f6f73828
ryuseongryong <ryuseongryong@gmail.com>
2022-07-27 23:54:49 +0900

    day 30 json data load, update, dump with try, except, finally

--- Udemy/Python_Angela/second_local/day30/main.py
+++ Udemy/Python_Angela/second_local/day30/main.py
@@ -99,15 +99,20 @@ def save():
         )
 
         if is_ok:
-            with open("data.json", "r") as data_file:
-                # Reading old data
-                data = json.load(data_file)
+            try:
+                with open("data.json", "r") as data_file:
+                    # Reading old data
+                    data = json.load(data_file)
+            except FileNotFoundError:
+                with open("data.json", "w") as data_file:
+                    json.dump(new_data, data_file, indent=4)
+            else:
                 # Updating old data with new data
                 data.update(new_data)
-            with open("data.json", "w") as data_file:
-                # Saving updated data
-                json.dump(data, data_file, indent=4)
-
+                with open("data.json", "w") as data_file:
+                    # Saving updated data
+                    json.dump(data, data_file, indent=4)
+            finally:
                 website_entry.delete(0, END)
                 password_entry.delete(0, END)
 
