b3f2b61e90c9e57335af17afdc5b6f0bf979ec36
ryuseongryong <ryuseongryong@gmail.com>
2022-07-28 21:01:12 +0900

    day 30 find password func with try, except

--- Udemy/Python_Angela/second_local/day30/main.py
+++ Udemy/Python_Angela/second_local/day30/main.py
@@ -117,6 +117,27 @@ def save():
                 password_entry.delete(0, END)
 
 
+# ---------------------------- FIND PASSWORD ------------------------------- #
+def find_password():
+    website = website_entry.get()
+    try:
+        with open("data.json") as data_file:
+            data = json.load(data_file)
+    except FileNotFoundError:
+        messagebox.showinfo(title="Error", message="No Data File Found!")
+    else:
+        if website in data:
+            email = data[website]["email"]
+            password = data[website]["password"]
+            messagebox.showinfo(
+                title=website, message=f"Email: {email}\nPassword: {password}"
+            )
+        else:
+            messagebox.showinfo(
+                title="Error", message=f"No details for {website} exists."
+            )
+
+
 # ---------------------------- UI SETUP ------------------------------- #
 
 window = Tk()
@@ -136,8 +157,8 @@ email_label.grid(row=2, column=0)
 password_label = Label(text="password")
 password_label.grid(row=3, column=0)
 
-website_entry = Entry(width=39)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1, columnspan=1)
 website_entry.focus()
 email_entry = Entry(width=39)
 email_entry.grid(row=2, column=1, columnspan=2)
@@ -145,7 +166,12 @@ email_entry.insert(0, "ryuseongryong@gmail.com")
 password_entry = Entry(width=21)
 password_entry.grid(row=3, column=1)
 
-generate_password_button = Button(text="Generate Password", command=generate_password)
+search_button = Button(text="Search", width=13, command=find_password)
+search_button.grid(row=1, column=2)
+
+generate_password_button = Button(
+    text="Generate Password", width=13, command=generate_password
+)
 generate_password_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, command=save)
