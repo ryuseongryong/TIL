4fc07e69c9eeb8dbdf8fe70bae16d0e935293709
ryuseongryong <ryuseongryong@gmail.com>
2022-07-29 01:01:38 +0900

    day 31 flashy word layout

---
+++ Udemy/Python_Angela/second_local/day31/flash-card-project-start/main.py
@@ -0,0 +1,31 @@
+from tkinter import *
+
+import os
+
+os.chdir("./Udemy/Python_Angela/second_local/day31/flash-card-project-start")
+# print(os.getcwd())
+
+BACKGROUND_COLOR = "#B1DDC6"
+
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
+
+
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.create_text(400, 150, text="Title", fill="Black", font=("Ariel", 40, "italic"))
+canvas.create_text(400, 264, text="word", fill="Black", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.grid(row=0, column=0, columnspan=2)
+
+cross_image = PhotoImage(file="images/wrong.png")
+unknown_button = Button(image=cross_image, highlightthickness=0)
+unknown_button.grid(row=1, column=0)
+
+check_image = PhotoImage(file="images/right.png")
+known_button = Button(image=check_image, highlightthickness=0)
+known_button.grid(row=1, column=1)
+
+window.mainloop()
