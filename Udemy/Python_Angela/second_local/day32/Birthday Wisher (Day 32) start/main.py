33a254c424c0e01f10163fc8526c76052c5c816c
ryuseongryong <ryuseongryong@gmail.com>
2022-08-03 00:18:22 +0900

    day 32 email smtp with python

--- Udemy/Python_Angela/second_local/day32/Birthday Wisher (Day 32) start/main.py
+++ Udemy/Python_Angela/second_local/day32/Birthday Wisher (Day 32) start/main.py
@@ -3,8 +3,11 @@ import smtplib
 my_email = "ryuseongryong@gmail.com"
 password = ""
 
-connection = smtplib.SMTP("smtp.gmail.com")
-connection.starttls()
-connection.login(user=my_email, password=password)
-connection.sendmail(from_addr=my_email, to_addrs=my_email, msg="Hello")
-connection.close()
+with smtplib.SMTP("smtp.gmail.com") as connection:
+    connection.starttls()
+    connection.login(user=my_email, password=password)
+    connection.sendmail(
+        from_addr=my_email,
+        to_addrs=my_email,
+        msg="Subject:Hello\n\nThis is the body of my email.",
+    )
