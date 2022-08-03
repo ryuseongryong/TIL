3b93b7e73d746d59b3f2316fe35b7d34e286b079
ryuseongryong <ryuseongryong@gmail.com>
2022-08-03 23:47:20 +0900

    day 32 datetime

--- Udemy/Python_Angela/second_local/day32/Birthday Wisher (Day 32) start/main.py
+++ Udemy/Python_Angela/second_local/day32/Birthday Wisher (Day 32) start/main.py
@@ -1,13 +1,23 @@
-import smtplib
+# import smtplib
 
-my_email = "ryuseongryong@gmail.com"
-password = ""
+# my_email = "ryuseongryong@gmail.com"
+# password = ""
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=password)
-    connection.sendmail(
-        from_addr=my_email,
-        to_addrs=my_email,
-        msg="Subject:Hello\n\nThis is the body of my email.",
-    )
+# with smtplib.SMTP("smtp.gmail.com") as connection:
+#     connection.starttls()
+#     connection.login(user=my_email, password=password)
+#     connection.sendmail(
+#         from_addr=my_email,
+#         to_addrs=my_email,
+#         msg="Subject:Hello\n\nThis is the body of my email.",
+#     )
+import datetime as dt
+
+now = dt.datetime.now()
+year = now.year
+month = now.month
+day_of_week = now.weekday()
+print(now, year, month, day_of_week)
+
+date_of_birth = dt.date(year=1993, month=9, day=25)
+print(date_of_birth)
