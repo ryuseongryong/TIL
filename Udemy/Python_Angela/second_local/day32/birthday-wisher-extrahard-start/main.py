9f4afc93089d8da2359fcac54c892dd2443add36
ryuseongryong <ryuseongryong@gmail.com>
2022-08-05 00:19:34 +0900

    day 32 happy birthday message automation fin

--- Udemy/Python_Angela/second_local/day32/birthday-wisher-extrahard-start/main.py
+++ Udemy/Python_Angela/second_local/day32/birthday-wisher-extrahard-start/main.py
@@ -2,7 +2,8 @@ import os, datetime, pandas, random, smtplib
 
 os.chdir("./Udemy/Python_Angela/second_local/day32/birthday-wisher-extrahard-start")
 
-
+MY_EMAIL = "ryuseongryong@gmail.com"
+MY_PASSWORD = "ubhxnclgbvivzuyt"
 
 
 now = datetime.datetime.now()
@@ -21,16 +22,13 @@ if today in birthdays_dict:
     file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
     with open(file_path) as letter_file:
         contents = letter_file.read()
-        contents.replace("[NAME]", birthday_person["name"])
+        contents = contents.replace("[NAME]", birthday_person["name"])
 
-    with smtplib.SMTP()
-
-
-##################### Extra Hard Starting Project ######################
-
-
-# 2. Check if today matches a birthday in the birthdays.csv
-
-# 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
-
-# 4. Send the letter generated in step 3 to that person's email address.
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=MY_EMAIL, password=MY_PASSWORD)
+        connection.sendmail(
+            from_addr=MY_EMAIL,
+            to_addrs=birthday_person["email"],
+            msg=f"Subject:Happy Birthday!!\n\n{contents}",
+        )
