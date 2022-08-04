9c19f2ab28ee30b89c5f32a51a701e280933b035
ryuseongryong <ryuseongryong@gmail.com>
2022-08-04 23:59:34 +0900

    day 32 happy birthday message automation read csv

--- Udemy/Python_Angela/second_local/day32/birthday-wisher-extrahard-start/main.py
+++ Udemy/Python_Angela/second_local/day32/birthday-wisher-extrahard-start/main.py
@@ -1,13 +1,29 @@
+import os, datetime, pandas, random
+
+os.chdir("./Udemy/Python_Angela/second_local/day32/birthday-wisher-extrahard-start")
+now = datetime.datetime.now()
+today = (now.month, now.day)
+
+
+data = pandas.read_csv("birthdays.csv")
+
+birthdays_dict = {
+    (data_row["month"], data_row["day"]): data_row
+    for (index, data_row) in data.iterrows()
+}
+
+if today in birthdays_dict:
+    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
+    with open(file_path) as letter_file:
+        contents = letter_file.read()
+        contents.replace("[NAME]", birthdays_dict[today])
+
+
 ##################### Extra Hard Starting Project ######################
 
-# 1. Update the birthdays.csv
 
 # 2. Check if today matches a birthday in the birthdays.csv
 
 # 3. If step 2 is true, pick a random letter from letter templates and replace the [NAME] with the person's actual name from birthdays.csv
 
 # 4. Send the letter generated in step 3 to that person's email address.
-
-
-
-
