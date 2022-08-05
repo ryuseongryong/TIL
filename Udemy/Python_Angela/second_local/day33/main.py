119d69d3bef79f3b433d50d68a1f05708c57ee57
ryuseongryong <ryuseongryong@gmail.com>
2022-08-05 23:45:38 +0900

    day 33 raise for status, JSON data

--- Udemy/Python_Angela/second_local/day33/main.py
+++ Udemy/Python_Angela/second_local/day33/main.py
@@ -1,4 +1,12 @@
 import requests
 
 res = requests.get(url="http://api.open-notify.org/iss-now.json")
-print(res)
+res.raise_for_status()
+
+data = res.json()
+
+longitude = data["iss_position"]["longitude"]
+latitude = data["iss_position"]["latitude"]
+
+iss_position = (latitude, longitude)
+print(iss_position)
