59cb4a0a22c2b96ff3c84773c7701a7473d51e17
ryuseongryong <ryuseongryong@gmail.com>
2022-08-15 23:12:12 +0900

    day 35 openweathermap check will rain in 12h

--- Udemy/Python_Angela/second_local/day35/main.py
+++ Udemy/Python_Angela/second_local/day35/main.py
@@ -10,7 +10,25 @@ API_KEY = os.getenv("API_KEY")
 LAT = os.getenv("LATITUDE")
 LONG = os.getenv("LONGITUDE")
 
-weather_params = {"lat": LAT, "lon": LONG, "appid": API_KEY}
+weather_params = {
+    "lat": LAT,
+    "lon": LONG,
+    "appid": API_KEY,
+    "exclude": "current,minutely,daily",
+}
 
 res = requests.get(OWN_Endpoint, params=weather_params)
-print(res.json())
+res.raise_for_status()
+weather_data = res.json()
+weather_slice = weather_data["hourly"][:12]
+
+
+will_rain = False
+
+for hour_data in weather_slice:
+    condition_code = int(hour_data["weather"][0]["id"])
+    if condition_code < 700:
+        will_rain = True
+
+if will_rain:
+    print("Bring an Umbrella!!")
