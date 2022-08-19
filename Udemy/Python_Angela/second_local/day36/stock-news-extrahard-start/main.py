a716e7928270ac5ee30fc54a0665d69fc9de009c
ryuseongryong <ryuseongryong@gmail.com>
2022-08-19 23:56:26 +0900

    day 36: Add yesterday closing price from stock data

--- Udemy/Python_Angela/second_local/day36/stock-news-extrahard-start/main.py
+++ Udemy/Python_Angela/second_local/day36/stock-news-extrahard-start/main.py
@@ -1,17 +1,40 @@
+import requests, os
+from dotenv import load_dotenv
+
+os.chdir("./Udemy/Python_Angela/second_local/day36")
+load_dotenv()
+
+
 STOCK = "TSLA"
 COMPANY_NAME = "Tesla Inc"
 
-## STEP 1: Use https://www.alphavantage.co
-# When STOCK price increase/decreases by 5% between yesterday and the day before yesterday then print("Get News").
+STOCK_ENDPOINT = "https://www.alphavantage.co/query"
+NEWS_ENDPOINT = "https://newsapi.org/v2/everything"
+
+STOCK_API_KEY = os.getenv("STOCK_API_KEY")
+
+stock_params = {
+    "function": "TIME_SERIES_DAILY",
+    "symbol": STOCK,
+    "apikey": STOCK_API_KEY,
+}
+
+res = requests.get(STOCK_ENDPOINT, params=stock_params)
+data = res.json()["Time Series (Daily)"]
+data_list = [val for (key, val) in data.items()]
+yesterday_data = data_list[0]
+yesterday_closing_price = yesterday_data["4. close"]
+print(yesterday_closing_price)
+
 
 ## STEP 2: Use https://newsapi.org
-# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME. 
+# Instead of printing ("Get News"), actually get the first 3 news pieces for the COMPANY_NAME.
 
 ## STEP 3: Use https://www.twilio.com
-# Send a seperate message with the percentage change and each article's title and description to your phone number. 
+# Send a seperate message with the percentage change and each article's title and description to your phone number.
 
 
-#Optional: Format the SMS message like this: 
+# Optional: Format the SMS message like this:
 """
 TSLA: 🔺2%
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
@@ -21,4 +44,3 @@ or
 Headline: Were Hedge Funds Right About Piling Into Tesla Inc. (TSLA)?. 
 Brief: We at Insider Monkey have gone over 821 13F filings that hedge funds and prominent investors are required to file by the SEC The 13F filings show the funds' and investors' portfolio positions as of March 31st, near the height of the coronavirus market crash.
 """
-
