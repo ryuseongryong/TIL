78ec011ce1713132ad5ddd859ef5123d10ea0cab
ryuseongryong <ryuseongryong@gmail.com>
2022-08-30 00:27:49 +0900

    day 38: Fix Sheety API

--- Udemy/Python_Angela/second_local/day38/main.py
+++ Udemy/Python_Angela/second_local/day38/main.py
@@ -13,6 +13,8 @@ AGE = "30"
 
 APP_ID = os.getenv("APP_ID")
 API_KEY = os.getenv("API_KEY")
+API_URL = os.getenv("API_URL")
+
 
 exercise_endpoint = "https://trackapi.nutritionix.com/v2/natural/exercise"
 
@@ -30,5 +32,22 @@ params = {
 
 res = requests.post(exercise_endpoint, json=params, headers=headers)
 print(res.json(), res)
+data = res.json()
 
-email_endpoint = "https://v2-api.sheety.co/phill/"
+today = datetime.now().strftime("%Y/%m/%d")
+now_time = datetime.now().strftime("%X")
+
+for exercise in data["exercises"]:
+    sheet_inputs = {
+        "workout": {
+            "date": today,
+            "time": now_time,
+            "exercise": exercise["name"].title(),
+            "duration": exercise["duration_min"],
+            "calories": exercise["nf_calories"],
+        }
+    }
+
+    sheet_res = requests.post(API_URL, json=sheet_inputs)
+
+    print(sheet_res.text)
