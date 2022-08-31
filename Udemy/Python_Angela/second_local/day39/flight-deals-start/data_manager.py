ee5fda6ca78c398ed618565324c1e5258bc6c5df
ryuseongryong <ryuseongryong@gmail.com>
2022-08-31 23:59:39 +0900

    day 39: Add sheety url

--- Udemy/Python_Angela/second_local/day39/flight-deals-start/data_manager.py
+++ Udemy/Python_Angela/second_local/day39/flight-deals-start/data_manager.py
@@ -1,3 +1,13 @@
+import requests, os
+from dotenv import load_dotenv
+
+os.chdir("./Udemy/Python_Angela/second_local/day39/filght-deals-start")
+load_dotenv()
+
+
+SHEETY_ENDPOINT = os.getenv("API_URL")
+
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
+    # This class is responsible for talking to the Google Sheet.
     pass
