09d96469d8875abe60810750da8e23c83c85ce72
ryuseongryong <ryuseongryong@gmail.com>
2022-09-01 00:06:01 +0900

    day 39: Add get from sheety url

--- Udemy/Python_Angela/second_local/day39/flight-deals-start/data_manager.py
+++ Udemy/Python_Angela/second_local/day39/flight-deals-start/data_manager.py
@@ -1,13 +1,26 @@
+from email import header
 import requests, os
 from dotenv import load_dotenv
 
-os.chdir("./Udemy/Python_Angela/second_local/day39/filght-deals-start")
+os.chdir("./Udemy/Python_Angela/second_local/day39/flight-deals-start")
 load_dotenv()
 
 
 SHEETY_ENDPOINT = os.getenv("API_URL")
+TOKEN = os.getenv("TOKEN")
+
+bearer_header = {"Authorization": TOKEN}
 
 
 class DataManager:
-    # This class is responsible for talking to the Google Sheet.
-    pass
+    def __init__(self) -> None:
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        res = requests.get(url=SHEETY_ENDPOINT, headers=bearer_header)
+        data = res.json()
+        return data
+
+
+a = DataManager().get_destination_data()
+print(a)
