e8a1e7944cb38d95a7137c159684d7d0bb5b580b
ryuseongryong <ryuseongryong@gmail.com>
2022-09-04 01:21:39 +0900

    day 39: Add search direct flight from ICN to Cities and costs

--- Udemy/Python_Angela/second_local/day39/flight-deals-start/flight_search.py
+++ Udemy/Python_Angela/second_local/day39/flight-deals-start/flight_search.py
@@ -1,5 +1,7 @@
 import os, requests
+from datetime import datetime, timedelta
 from dotenv import load_dotenv
+from flight_data import FlightData
 
 load_dotenv()
 
@@ -19,3 +21,43 @@ class FlightSearch:
         data = res.json()["locations"]
         iataCode = data[0]["code"]
         return iataCode
+
+    def check_flights(
+        self, origin_city_code, destination_city_code, from_time, to_time
+    ):
+        headers = {"apikey": TEQUILA_API_KEY}
+        query = {
+            "fly_from": origin_city_code,
+            "fly_to": destination_city_code,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "KRW",
+        }
+
+        res = requests.get(
+            url=f"{TEQUILA_ENDPOINT}/v2/search", params=query, headers=headers
+        )
+        print(res.text)
+
+        try:
+            data = res.json()["data"][0]
+        except IndexError:
+            print(f"No Flights found for {destination_city_code}!")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][0]["local_departure"].split("T")[0],
+        )
+        print(f"{flight_data.destination_city}: ₩{flight_data.price}")
+        return flight_data
--- Udemy/Python_Angela/second_local/day39/flight-deals-start/main.py
+++ Udemy/Python_Angela/second_local/day39/flight-deals-start/main.py
@@ -1,21 +1,25 @@
+from datetime import datetime, timedelta
 from data_manager import DataManager
+from flight_search import FlightSearch
 
-print("start main")
 data_manager = DataManager()
+flight_search = FlightSearch()
 sheet_data = data_manager.get_destination_data()
 
 if sheet_data[0]["iataCode"] == "Test":
-
     for row in sheet_data:
-        from flight_search import FlightSearch
-
-        flight_search = FlightSearch()
-
-        print(flight_search.get_destination_code(row["city"]))
-
         row["iataCode"] = flight_search.get_destination_code(row["city"])
     print(f"sheet_data:\n {sheet_data}")
-
     data_manager.destination_data = sheet_data
     data_manager.update_destination_iatacode()
-print("end of main")
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.check_flights(
+        origin_city_code="ICN",
+        destination_city_code=destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today,
+    )
