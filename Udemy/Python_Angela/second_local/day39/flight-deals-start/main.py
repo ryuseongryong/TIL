7e2c8441cfb6d2eb304fdb43b56eedc2dc751ae9
ryuseongryong <ryuseongryong@gmail.com>
2022-09-04 23:59:55 +0900

    day 39: Add sms

--- Udemy/Python_Angela/second_local/day39/flight-deals-start/main.py
+++ Udemy/Python_Angela/second_local/day39/flight-deals-start/main.py
@@ -1,9 +1,11 @@
 from datetime import datetime, timedelta
 from data_manager import DataManager
 from flight_search import FlightSearch
+from notification_manager import NotificationManager
 
 data_manager = DataManager()
 flight_search = FlightSearch()
+notification_manager = NotificationManager()
 sheet_data = data_manager.get_destination_data()
 
 if sheet_data[0]["iataCode"] == "Test":
@@ -23,3 +25,7 @@ for destination in sheet_data:
         from_time=tomorrow,
         to_time=six_month_from_today,
     )
+    if flight.price < destination["lowestPrice"]:
+        notification_manager.send_msg(
+            msg=f"Low price alert! Only {flight.price} to fly from {flight.origin_city}-{flight.origin_airport} to {flight.destination_city}-{flight.destination_airport}, from {flight.out_date} to {flight.return_date}."
+        )
