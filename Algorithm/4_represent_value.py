80c9b8c9bc004dc3abc85842cd478438141c8693
ryuseongryong <ryuseongryong@gmail.com>
2022-09-06 23:58:06 +0900

    Algorithm 4 represent_value : Fix round function - round half even

--- Algorithm/4_represent_value.py
+++ Algorithm/4_represent_value.py
@@ -1,5 +1,6 @@
 def solution(n, val_list):
-    avg_val = round(sum(val_list) / n)
+    avg_val = sum(val_list) / n
+    avg_val = int(avg_val + 0.5)
     min_val = (2**31) - 1
     for idx, val in enumerate(val_list):
         temp = abs(val - avg_val)
@@ -12,3 +13,13 @@ def solution(n, val_list):
                 score = val
                 index = idx + 1
     print(avg_val, index)
+
+
+"""
+round는 round_half_even 방식
+round(4.5) == 4
+round(5.5) == 6
+
+따라서 round대신 int() +0.5를 사용하는 것이 더 정확하다.
+"""
+print(round(5.5))
