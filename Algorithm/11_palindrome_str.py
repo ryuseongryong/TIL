d7ca0abf5fff6117aa78d7623f5bab14011f7ef0
ryuseongryong <ryuseongryong@gmail.com>
2022-09-17 00:09:49 +0900

    Algorithm 11 palindrome Fix

--- Algorithm/11_palindrome_str.py
+++ Algorithm/11_palindrome_str.py
@@ -1,11 +1,25 @@
 def solution(num, str_list):
+    # for i in range(num):
+    #     str = str_list[i]
+    #     str = str.upper()
+    #     if str == str[::-1]:
+    #         print(f"#{(i+1)} YES")
+    #     else:
+    #         print(f"#{(i+1)} NO")
+
     for i in range(num):
-        str = str_list[0]
-        str = str.upper()
-        if str == str[::-1]:
-            print(f"#{(i+1)} YES")
+        s = str_list[i]
+        s = s.upper()
+        size = len(s)
+        for j in range(size // 2):
+            if s[j] != s[-1 - j]:
+                print(f"#{i+1} NO")
+                break
         else:
-            print(f"#{(i+1)} NO")
+            print(f"{i+1} YES")
 
 
-solution(5, ["level"])
+solution(
+    5,
+    ["level", "moon", "abcba", "soon", "gooG"],
+)
