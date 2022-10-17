45b194ac917bd6dbc2db2a7d8ccb2ca7a9abb596
ryuseongryong <ryuseongryong@gmail.com>
2022-10-17 16:56:08 +0900

    day 45: Add web scraping class_

--- Udemy/Python_Angela/second_local/day45/bs4-start/main.py
+++ Udemy/Python_Angela/second_local/day45/bs4-start/main.py
@@ -15,4 +15,31 @@ soup = BeautifulSoup(contents, "html.parser")
 
 # print(soup.prettify())
 
-print(soup.p)
+# print(soup.p)
+
+all_anchor_tags = soup.find_all(name="a")
+# print(all_anchor_tags)
+
+for tag in all_anchor_tags:
+    # print(tag.getText())
+    # print(tag.get("href"))
+    pass
+
+heading = soup.find(name="h1", id="name")
+# print(heading)
+
+section_heading = soup.find(name="h3", class_="heading")
+# print(section_heading.getText())
+# print(section_heading.get("class"))
+
+class_is_heading = soup.find_all(class_="heading")
+print(class_is_heading)
+
+h3_heading = soup.find_all("h3", class_="heading")
+print(h3_heading)
+
+name = soup.select_one(selector="#name")
+print(name)
+
+headings = soup.select(".heading")
+print(headings)
