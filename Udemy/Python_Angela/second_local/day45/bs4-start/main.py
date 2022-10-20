a407b3c331c79fd950adb0027797754390895817
ryuseongryong <ryuseongryong@gmail.com>
2022-10-20 23:58:14 +0900

    day 45: Add get all values from scraping

--- Udemy/Python_Angela/second_local/day45/bs4-start/main.py
+++ Udemy/Python_Angela/second_local/day45/bs4-start/main.py
@@ -10,11 +10,23 @@ res = requests.get("https://news.ycombinator.com/news")
 yc_web_page = res.text
 
 soup = BeautifulSoup(yc_web_page, "html.parser")
-print(yc_web_page)
+# print(yc_webs_page)
 # print(soup.title)
-article_tag = soup.find(name="span", class_="titleline").find(name="a")
-article_text = article_tag.getText()
-print(article_text)
+articles = soup.find_all(name="span", class_="titleline").find_all(name="a")
+article_texts = []
+article_links = []
+for article_tag in articles:
+    text = article_tag.getText()
+    article_texts.append(text)
+    link = article_tag.get("href")
+    article_links.append(link)
+
+article_upvote = [
+    score.getText() for score in soup.find_all(name="span", class_="score")
+]
+print(article_texts)
+print(article_links)
+print(article_upvote)
 
 
 # -------------------------------------------------------------------------------------------------------- #
