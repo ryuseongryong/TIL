9554fcd75a4135c7aacf18cc5783f8583b8d6a2e
ryuseongryong <ryuseongryong@gmail.com>
2022-10-21 00:15:58 +0900

    day 45: Add largest votes text, link

--- Udemy/Python_Angela/second_local/day45/bs4-start/main.py
+++ Udemy/Python_Angela/second_local/day45/bs4-start/main.py
@@ -26,11 +26,17 @@ for article_tag in articles:
     article_links.append(link)
 
 article_upvote = [
-    score.getText() for score in soup.find_all(name="span", class_="score")
+    int(score.getText().split()[0])
+    for score in soup.find_all(name="span", class_="score")
 ]
-print(article_texts)
-print(article_links)
-print(article_upvote)
+
+largest_num = max(article_upvote)
+largest_idx = article_upvote.index(largest_num)
+print(article_texts[largest_idx])
+print(article_links[largest_idx])
+# print(article_texts)
+# print(article_links)
+# print(article_upvote)
 
 
 # -------------------------------------------------------------------------------------------------------- #
