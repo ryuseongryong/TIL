beed224c0e675000181821bf890e4121980d0365
ryuseongryong <ryuseongryong@gmail.com>
2022-10-22 23:00:06 +0900

    day 45: Add 100 movies scraping

--- Udemy/Python_Angela/second_local/day45/Starting Code - 100 movies to watch start/main.py
+++ Udemy/Python_Angela/second_local/day45/Starting Code - 100 movies to watch start/main.py
@@ -1,8 +1,24 @@
-import requests
+import requests, os
 from bs4 import BeautifulSoup
+
+os.chdir(
+    "./Udemy/Python_Angela/second_local/day45/Starting Code - 100 movies to watch start"
+)
 
 URL = "https://web.archive.org/web/20200518073855/https://www.empireonline.com/movies/features/best-movies-2/"
 
 # Write your code below this line 👇
 
+res = requests.get(URL)
+website_html = res.text
 
+soup = BeautifulSoup(website_html, "html.parser")
+
+all_movies = soup.find_all(name="h3", class_="title")
+
+movie_titles = [movie.getText() for movie in all_movies]
+movies = movie_titles[::-1]
+
+with open("movies.txt", mode="w") as file:
+    for movie in movies:
+        file.write(f"{movie}\n")
