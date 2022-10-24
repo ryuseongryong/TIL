30618acb2005d5e40b0e604d9255c9e2ebb151b9
ryuseongryong <ryuseongryong@gmail.com>
2022-10-24 23:32:26 +0900

    day 46: Add Spotipy OAuth

--- Udemy/Python_Angela/second_local/day46/main.py
+++ Udemy/Python_Angela/second_local/day46/main.py
@@ -1,14 +1,38 @@
 import os, requests
+from dotenv import load_dotenv
 from bs4 import BeautifulSoup
 
 os.chdir("./Udemy/Python_Angela/second_local/day46")
+load_dotenv()
 
-date = input(
-    "Which year do you want to travel to? Type the date in this format YYYY-MM-DD: "
+# date = input(
+#     "Which year do you want to travel to? Type the date in this format YYYY-MM-DD: "
+# )
+
+# res = requests.get("https://www.billboard.com/charts/hot-100/" + date)
+
+# soup = BeautifulSoup(res.text, "html.parser")
+# song_name_spans = soup.find_all("span", class_="a-no-trucate")
+# song_name = [song.getText() for song in song_name_spans]
+
+# ----------------------------------------------------------------------------------------
+
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+
+sp = spotipy.Spotify(
+    auth_manager=SpotifyOAuth(
+        scope="playlist-read-private",
+        redirect_uri="https://example.com/callback",
+        client_id=CLIENT_ID,
+        client_secret=CLIENT_SECRET,
+        show_dialog=True,
+        cache_path="token.txt",
+    )
 )
 
-res = requests.get("https://www.billboard.com/charts/hot-100/" + date)
-
-soup = BeautifulSoup(res.text, "html.parser")
-song_name_spans = soup.find_all("span", class_="a-no-trucate")
-song_name = [song.getText() for song in song_name_spans]
+user_id = sp.current_user()["id"]
+print(user_id)
