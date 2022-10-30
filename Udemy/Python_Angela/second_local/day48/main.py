a0d3414e9d6f9ae969f05a061255b07b65fffeeb
ryuseongryong <ryuseongryong@gmail.com>
2022-10-31 00:04:32 +0900

    day 48: Add events dictionaries

--- Udemy/Python_Angela/second_local/day48/main.py
+++ Udemy/Python_Angela/second_local/day48/main.py
@@ -29,7 +29,16 @@ driver.get("https://www.python.org/")
 # documentation_link = driver.find_element(By.CSS_SELECTOR, ".documentation-widget a")
 # print(documentation_link.text)
 
-bug_link = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
-print(bug_link.text)
+# bug_link = driver.find_element(By.XPATH, '//*[@id="site-map"]/div[2]/div/ul/li[3]/a')
+# print(bug_link.text)
+
+event_times = driver.find_elements(By.CSS_SELECTOR, ".event-widget time")
+event_names = driver.find_elements(By.CSS_SELECTOR, ".event-widget li a")
+events = {}
+
+for n in range(len(event_times)):
+    events[n] = {"time": event_times[n].text, "name": event_names[n].text}
+
+print(events)
 
 driver.quit()
