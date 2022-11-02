2a85b47be533f23de1735ae49a5d43b874124068
ryuseongryong <ryuseongryong@gmail.com>
2022-11-02 23:41:26 +0900

    day 48: Add automatic interaction

--- Udemy/Python_Angela/second_local/day48/interaction.py
+++ Udemy/Python_Angela/second_local/day48/interaction.py
@@ -2,6 +2,7 @@ import os
 from dotenv import load_dotenv
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 load_dotenv()
@@ -12,6 +13,13 @@ driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
 
 driver.get("https://ko.wikipedia.org/wiki/Main_Page")
 article_count = driver.find_element(By.CSS_SELECTOR, ".nomobile a")
-print(article_count.text)
+# article_count.click()
+
+community = driver.find_element(By.LINK_TEXT, "사랑방")
+# community.click()
+
+search = driver.find_element(By.NAME, "search")
+search.send_keys("Python")
+search.send_keys(Keys.ENTER)
 
 driver.quit()
