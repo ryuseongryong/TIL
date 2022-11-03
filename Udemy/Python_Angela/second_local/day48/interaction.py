c4ce8c00ed67e3bb2a5148ed3893fb5c265c26a5
ryuseongryong <ryuseongryong@gmail.com>
2022-11-03 22:42:05 +0900

    day 48: Add form automation

--- Udemy/Python_Angela/second_local/day48/interaction.py
+++ Udemy/Python_Angela/second_local/day48/interaction.py
@@ -11,15 +11,27 @@ os.chdir("./Udemy/Python_Angela/second_local/day48")
 CHROME_DRIVER_PATH = os.getenv("CHROME_DRIVER_PATH")
 driver = webdriver.Chrome(executable_path=CHROME_DRIVER_PATH)
 
-driver.get("https://ko.wikipedia.org/wiki/Main_Page")
-article_count = driver.find_element(By.CSS_SELECTOR, ".nomobile a")
+# driver.get("https://ko.wikipedia.org/wiki/Main_Page")
+driver.get("https://secure-retreat-92358.herokuapp.com/")
+
+first_name = driver.find_element(By.NAME, "fName")
+first_name.send_keys("Seongryong")
+last_name = driver.find_element(By.NAME, "lName")
+last_name.send_keys("Ryu")
+email = driver.find_element(By.NAME, "email")
+email.send_keys("ryuseongryong@gmail.com")
+
+submit = driver.find_element(By.CSS_SELECTOR, "form button")
+submit.click()
+
+# article_count = driver.find_element(By.CSS_SELECTOR, ".nomobile a")
 # article_count.click()
 
-community = driver.find_element(By.LINK_TEXT, "사랑방")
+# community = driver.find_element(By.LINK_TEXT, "사랑방")
 # community.click()
 
-search = driver.find_element(By.NAME, "search")
-search.send_keys("Python")
-search.send_keys(Keys.ENTER)
+# search = driver.find_element(By.NAME, "search")
+# search.send_keys("Python")
+# search.send_keys(Keys.ENTER)
 
-driver.quit()
+# driver.quit()
