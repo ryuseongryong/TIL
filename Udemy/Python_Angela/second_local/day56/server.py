8b42ef19f0202330e99af57fc4e078970c42aebf
ryuseongryong <ryuseongryong@gmail.com>
2022-11-21 00:03:14 +0900

    day 56: Add Flask template HTML

--- Udemy/Python_Angela/second_local/day56/server.py
+++ Udemy/Python_Angela/second_local/day56/server.py
@@ -6,6 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def home():
     return render_template("index.html")
+    # return render_template("index.html")
     # return render_template("seongryong.html")
 
 
