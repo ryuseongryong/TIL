104d25f6a99b47e67c90f8b46cbb72233f67ed11
ryuseongryong <ryuseongryong@gmail.com>
2022-12-01 15:46:02 +0900

    day 63: Add 'Add New Book' function and routing

--- Udemy/Python_Angela/second_local/day63/Starting+Files+-+library-start/main.py
+++ Udemy/Python_Angela/second_local/day63/Starting+Files+-+library-start/main.py
@@ -5,16 +5,25 @@ app = Flask(__name__)
 all_books = []
 
 
-@app.route('/')
+@app.route("/")
 def home():
-    pass
+    return render_template("index.html", all_books=all_books)
 
 
-@app.route("/add")
+@app.route("/add", methods=["GET", "POST"])
 def add():
-    pass
+    if request.method == "POST":
+        new_book = {
+            "title": request.form["title"],
+            "author": request.form["author"],
+            "rating": request.form["rating"],
+        }
+        all_books.append(new_book)
+
+        return redirect(url_for("home"))
+
+    return render_template("add.html")
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
