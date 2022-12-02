0666b4d8ada6426e58eb1a32c258a525f19cf30e
ryuseongryong <ryuseongryong@gmail.com>
2022-12-02 23:57:57 +0900

    day 63: Add sqlite db

---
+++ Udemy/Python_Angela/second_local/day63/sqlite/main.py
@@ -0,0 +1,10 @@
+import sqlite3
+import os
+
+os.chdir("./Udemy/Python_Angela/second_local/day63/sqlite")
+
+db = sqlite3.connect("books-collection.db")
+cursor = db.cursor()
+cursor.execute(
+    "CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)"
+)
