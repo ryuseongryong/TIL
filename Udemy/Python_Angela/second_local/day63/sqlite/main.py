43510056f7258d3740714a01c8bcbc13622386f2
ryuseongryong <ryuseongryong@gmail.com>
2022-12-03 00:13:00 +0900

    day 63: Add sqlite data

--- Udemy/Python_Angela/second_local/day63/sqlite/main.py
+++ Udemy/Python_Angela/second_local/day63/sqlite/main.py
@@ -5,6 +5,9 @@ os.chdir("./Udemy/Python_Angela/second_local/day63/sqlite")
 
 db = sqlite3.connect("books-collection.db")
 cursor = db.cursor()
-cursor.execute(
-    "CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)"
-)
+# cursor.execute(
+#     "CREATE TABLE books (id INTEGER PRIMARY KEY, title varchar(250) NOT NULL UNIQUE, author varchar(250) NOT NULL, rating FLOAT NOT NULL)"
+# )
+
+cursor.execute("INSERT INTO books VALUES(1, 'Harry Potter', 'J. K. Rowling', '9.9')")
+db.commit()
