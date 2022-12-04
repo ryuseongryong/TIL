08d869b0110a97f376df082a1d2af5a7f5a13914
ryuseongryong <ryuseongryong@gmail.com>
2022-12-04 14:34:20 +0900

    day 63: TODO sql unable to open database file

--- Udemy/Python_Angela/second_local/day63/sqlite/main.py
+++ Udemy/Python_Angela/second_local/day63/sqlite/main.py
@@ -4,7 +4,7 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 os.chdir("./Udemy/Python_Angela/second_local/day63/sqlite")
-
+file_path = os.path.abspath(os.getcwd()) + "/books-collection.db"
 # db = sqlite3.connect("books-collection.db")
 # cursor = db.cursor()
 # # cursor.execute(
@@ -16,23 +16,24 @@ os.chdir("./Udemy/Python_Angela/second_local/day63/sqlite")
 
 app = Flask(__name__)
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://new-books-collection.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://" + file_path
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
 
 
-class Book(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(250), unique=True, nullable=False)
-    author = db.Column(db.String(250), nullable=False)
-    rating = db.Column(db.Float, nullable=False)
+with app.app_context():
 
-    def __repr__(self) -> str:
-        return f"<Books {self.title}>"
+    class Book(db.Model):
+        id = db.Column(db.Integer, primary_key=True)
+        title = db.Column(db.String(250), unique=True, nullable=False)
+        author = db.Column(db.String(250), nullable=False)
+        rating = db.Column(db.Float, nullable=False)
 
+        def __repr__(self):
+            return f"<Book {self.title}>"
 
-db.create_all()
+    db.create_all()
 
-new_book = Book(id=1, title="Harry Potter2", author="J. K. Rowling", rating=9.3)
-db.session.add(new_book)
-db.session.commit()
+    new_book = Book(id=1, title="Harry Potter2", author="J. K. Rowling", rating=9.3)
+    db.session.add(new_book)
+    db.session.commit()
