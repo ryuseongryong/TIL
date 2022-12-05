8c196cf4391bcb645055a85a2674b07d88bd3846
ryuseongryong <ryuseongryong@gmail.com>
2022-12-05 23:29:08 +0900

    day 63: Fix sql unable to open database file

--- Udemy/Python_Angela/second_local/day63/sqlite/main.py
+++ Udemy/Python_Angela/second_local/day63/sqlite/main.py
@@ -4,7 +4,6 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 os.chdir("./Udemy/Python_Angela/second_local/day63/sqlite")
-file_path = os.path.abspath(os.getcwd()) + "/books-collection.db"
 # db = sqlite3.connect("books-collection.db")
 # cursor = db.cursor()
 # # cursor.execute(
@@ -15,25 +14,37 @@ file_path = os.path.abspath(os.getcwd()) + "/books-collection.db"
 # db.commit()
 
 app = Flask(__name__)
+app.secret_key = "lkjsldkjfksdf454854dfs"
 
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://" + file_path
+# app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite://" + file_path
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///new-books-collection.db"
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
-db = SQLAlchemy(app)
+db = SQLAlchemy(app=app)
+
+
+class Book(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(250), unique=True, nullable=False)
+    author = db.Column(db.String(250), nullable=False)
+    rating = db.Column(db.Float, nullable=False)
+
+    def __init__(self, id, title, author, rating):
+        self.id = id
+        self.title = title
+        self.author = author
+        self.rating = rating
+
+    def __repr__(self):
+        return f"<Book {self.title}>"
 
 
 with app.app_context():
-
-    class Book(db.Model):
-        id = db.Column(db.Integer, primary_key=True)
-        title = db.Column(db.String(250), unique=True, nullable=False)
-        author = db.Column(db.String(250), nullable=False)
-        rating = db.Column(db.Float, nullable=False)
-
-        def __repr__(self):
-            return f"<Book {self.title}>"
 
     db.create_all()
 
     new_book = Book(id=1, title="Harry Potter2", author="J. K. Rowling", rating=9.3)
     db.session.add(new_book)
     db.session.commit()
+
+# if __name__ == "__main__":
+#     app.run(debug=True)
