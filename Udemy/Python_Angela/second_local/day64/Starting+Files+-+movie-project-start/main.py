290f78c64907f59088799202420db548948ceb47
ryuseongryong <ryuseongryong@gmail.com>
2022-12-10 23:57:13 +0900

    day 64: Add rating movie

--- Udemy/Python_Angela/second_local/day64/Starting+Files+-+movie-project-start/main.py
+++ Udemy/Python_Angela/second_local/day64/Starting+Files+-+movie-project-start/main.py
@@ -32,29 +32,50 @@ class Movie(db.Model):
     img_url = db.Column(db.String(250), nullable=False)
 
 
+class RateMovieForm(FlaskForm):
+    rating = StringField("Your Rating Out of 10 e.g. 7.7")
+    review = StringField("Your Review")
+    submit = SubmitField("Done")
+
+
 with app.app_context():
     db.create_all()
 
 
 ## After adding the new_movie the code needs to be commented out/deleted.
 ## So you are not trying to add the same movie twice.
-# new_movie = Movie(
-#     title="Phone Booth",
-#     year=2002,
-#     description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
-#     rating=7.3,
-#     ranking=10,
-#     review="My favourite character was the caller.",
-#     img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg"
-# )
-# db.session.add(new_movie)
-# db.session.commit()
+# with app.app_context():
+#     new_movie = Movie(
+#         title="Phone Booth",
+#         year=2002,
+#         description="Publicist Stuart Shepard finds himself trapped in a phone booth, pinned down by an extortionist's sniper rifle. Unable to leave or receive outside help, Stuart's negotiation with the caller leads to a jaw-dropping climax.",
+#         rating=7.3,
+#         ranking=10,
+#         review="My favourite character was the caller.",
+#         img_url="https://image.tmdb.org/t/p/w500/tjrX2oWRCM3Tvarz38zlZM7Uc10.jpg",
+#     )
+#     db.session.add(new_movie)
+#     db.session.commit()
 
 
 @app.route("/")
 def home():
     all_movies = Movie.query.all()
     return render_template("index.html", movies=all_movies)
+
+
+@app.route("/edit", methods=["GET", "POST"])
+def rate_movie():
+    form = RateMovieForm()
+    movie_id = request.args.get("id")
+    movie = Movie.query.get(movie_id)
+    if form.validate_on_submit():
+        movie.rating = float(form.rating.data)
+        movie.review = form.review.data
+        with app.app_context():
+            db.session.commit()
+            return redirect(url_for("home"))
+    return render_template("edit.html", movie=movie, form=form)
 
 
 if __name__ == "__main__":
