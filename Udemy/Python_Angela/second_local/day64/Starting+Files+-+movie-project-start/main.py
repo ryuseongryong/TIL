0b9ad45802322142e39e5125b8cf7193b2ef43a7
ryuseongryong <ryuseongryong@gmail.com>
2022-12-12 23:58:53 +0900

    day 64: Add adding movie

--- Udemy/Python_Angela/second_local/day64/Starting+Files+-+movie-project-start/main.py
+++ Udemy/Python_Angela/second_local/day64/Starting+Files+-+movie-project-start/main.py
@@ -5,10 +5,17 @@ from flask_wtf import FlaskForm
 from wtforms import StringField, SubmitField
 from wtforms.validators import DataRequired
 import requests, os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # os.chdir(
 #     "./Udemy/Python_Angela/second_local/day64/Starting+Files+-+movie-project-start"
 # )
+MOVIE_DB_API_KEY = os.getenv("MOVIE_DB_API_KEY")
+MOVIE_DB_SEARCH_URL = "https://api.themoviedb.org/3/search/movie"
+MOVIE_DB_INFO_URL = "https://api.themoviedb.org/3/movie"
+MOVIE_DB_IMAGE_URL = "https://image.tmdb.org/t/p/w500"
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = "8BYkEfBA6O6donzWlSihBXox7C0sKR6b"
@@ -36,6 +43,11 @@ class RateMovieForm(FlaskForm):
     rating = StringField("Your Rating Out of 10 e.g. 7.7")
     review = StringField("Your Review")
     submit = SubmitField("Done")
+
+
+class FindMovieForm(FlaskForm):
+    title = StringField("Movie Title", validators=[DataRequired()])
+    submit = SubmitField("Add Movie")
 
 
 with app.app_context():
@@ -76,6 +88,22 @@ def rate_movie():
             db.session.commit()
             return redirect(url_for("home"))
     return render_template("edit.html", movie=movie, form=form)
+
+
+@app.route("/add", methods=["GET", "POST"])
+def add_movie():
+    form = FindMovieForm()
+
+    if form.validate_on_submit():
+        movie_title = form.title.data
+        response = requests.get(
+            MOVIE_DB_SEARCH_URL,
+            params={"api_key": MOVIE_DB_API_KEY, "query": movie_title},
+        )
+        data = response.json()["results"]
+        return render_template("select.html", options=data)
+
+    return render_template("add.html", form=form)
 
 
 @app.route("/delete")
