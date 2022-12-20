60d430664a8c7c3321cdaabf24f17dc48f05d0ad
ryuseongryong <ryuseongryong@gmail.com>
2022-12-20 23:53:44 +0900

    day 66: Add adding cafe

--- Udemy/Python_Angela/second_local/day66/Starting+Files+-+cafe-api-start/main.py
+++ Udemy/Python_Angela/second_local/day66/Starting+Files+-+cafe-api-start/main.py
@@ -81,6 +81,25 @@ def get_cafe_at_location():
         )
 
 
+@app.route("/add", methods=["POST"])
+def post_new_cafe():
+    new_cafe = Cafe(
+        name=request.form.get("name"),
+        map_url=request.form.get("map_url"),
+        img_url=request.form.get("img_url"),
+        location=request.form.get("loc"),
+        has_sockets=bool(request.form.get("sockets")),
+        has_toilet=bool(request.form.get("toilet")),
+        has_wifi=bool(request.form.get("wifi")),
+        can_take_calls=bool(request.form.get("calls")),
+        seats=request.form.get("seats"),
+        coffee_price=request.form.get("coffee_price"),
+    )
+    db.session.add(new_cafe)
+    db.session.commit()
+    return jsonify(response={"success": "Successfully added the new cafe."})
+
+
 ## HTTP GET - Read Record
 
 ## HTTP POST - Create Record
