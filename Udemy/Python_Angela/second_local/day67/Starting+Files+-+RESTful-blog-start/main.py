c37679a744fb158fe2b6c263a1516486e65773f0
ryuseongryong <ryuseongryong@gmail.com>
2022-12-26 12:19:28 +0900

    day 67: Add edit posts

--- Udemy/Python_Angela/second_local/day67/Starting+Files+-+RESTful-blog-start/main.py
+++ Udemy/Python_Angela/second_local/day67/Starting+Files+-+RESTful-blog-start/main.py
@@ -76,6 +76,27 @@ def add_new_post():
     return render_template("make-post.html", form=form)
 
 
+@app.route("/edit-post/<int:post_id>", methods=["GET", "POST"])
+def edit_post(post_id):
+    post = BlogPost.query.get(post_id)
+    edit_form = CreatePostForm(
+        title=post.title,
+        subtitle=post.subtitle,
+        img_url=post.img_url,
+        author=post.author,
+        body=post.body,
+    )
+    if edit_form.validate_on_submit():
+        post.title = edit_form.title.data
+        post.subtitle = edit_form.subtitle.data
+        post.img_url = edit_form.img_url.data
+        post.author = edit_form.author.data
+        post.body = edit_form.body.data
+        db.session.commit()
+        return redirect(url_for("show_post", post_id=post.id))
+    return render_template("make-post.html", form=edit_form, is_edit=True)
+
+
 @app.route("/about")
 def about():
     return render_template("about.html")
