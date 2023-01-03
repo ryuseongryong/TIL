5519a5bb475f58a19dd998adb45e5cbc1b195db9
ryuseongryong <ryuseongryong@gmail.com>
2023-01-03 23:01:37 +0900

    day 68: Add message

--- Udemy/Python_Angela/second_local/day68/Starting+Files+-+flask-auth-start/main.py
+++ Udemy/Python_Angela/second_local/day68/Starting+Files+-+flask-auth-start/main.py
@@ -57,6 +57,11 @@ def home():
 def register():
     if request.method == "POST":
 
+        if User.query.filter_by(email=request.form.get("email")).first():
+            # User already exists
+            flash("You've already signed up with that email, log in instead!")
+            return redirect(url_for("login"))
+
         hash_and_salted_password = generate_password_hash(
             request.form.get("password"), method="pbkdf2:sha256", salt_length=8
         )
@@ -65,28 +70,34 @@ def register():
             name=request.form.get("name"),
             password=hash_and_salted_password,
         )
-
         db.session.add(new_user)
         db.session.commit()
-
         login_user(new_user)
-
         return redirect(url_for("secrets"))
 
     return render_template("register.html")
 
 
-@app.route("/login")
+@app.route("/login", methods=["GET", "POST"])
 def login():
     if request.method == "POST":
         email = request.form.get("email")
         password = request.form.get("password")
 
         user = User.query.filter_by(email=email).first()
-
-        if check_password_hash(user.password, password):
+        # Email doesn't exist
+        if not user:
+            flash("That email does not exist, please try again.")
+            return redirect(url_for("login"))
+        # Password incorrect
+        elif not check_password_hash(user.password, password):
+            flash("Password incorrect, please try again.")
+            return redirect(url_for("login"))
+        # Email exists and password correct
+        else:
             login_user(user)
             return redirect(url_for("secrets"))
+
     return render_template("login.html")
 
 
