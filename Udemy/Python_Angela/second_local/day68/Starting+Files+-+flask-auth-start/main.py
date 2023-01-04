6200f02c8ebe317e506f10419696f42aeab39dac
ryuseongryong <ryuseongryong@gmail.com>
2023-01-04 22:07:23 +0900

    day 68: Add auth to html

--- Udemy/Python_Angela/second_local/day68/Starting+Files+-+flask-auth-start/main.py
+++ Udemy/Python_Angela/second_local/day68/Starting+Files+-+flask-auth-start/main.py
@@ -27,15 +27,6 @@ app.config["SECRET_KEY"] = "any-secret-key-you-choose"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///" + os.path.join(basedir, "users.db")
 app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
 db = SQLAlchemy(app)
-
-##CREATE TABLE IN DB
-class User(UserMixin, db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String(100), unique=True)
-    password = db.Column(db.String(100))
-    name = db.Column(db.String(1000))
-
-
 login_manager = LoginManager()
 login_manager.init_app(app)
 
@@ -45,12 +36,20 @@ def load_user(user_id):
     return User.query.get(int(user_id))
 
 
+##CREATE TABLE
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+
 # db.create_all()
 
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    return render_template("index.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/register", methods=["GET", "POST"])
@@ -75,7 +74,7 @@ def register():
         login_user(new_user)
         return redirect(url_for("secrets"))
 
-    return render_template("register.html")
+    return render_template("register.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/login", methods=["GET", "POST"])
@@ -85,39 +84,38 @@ def login():
         password = request.form.get("password")
 
         user = User.query.filter_by(email=email).first()
-        # Email doesn't exist
+        # Email doesn't exist or password incorrect.
         if not user:
             flash("That email does not exist, please try again.")
             return redirect(url_for("login"))
-        # Password incorrect
         elif not check_password_hash(user.password, password):
             flash("Password incorrect, please try again.")
             return redirect(url_for("login"))
-        # Email exists and password correct
         else:
             login_user(user)
             return redirect(url_for("secrets"))
 
-    return render_template("login.html")
+    return render_template("login.html", logged_in=current_user.is_authenticated)
 
 
 @app.route("/secrets")
+@login_required
 def secrets():
     print(current_user.name)
-    return render_template("secrets.html", name=current_user.name)
+    return render_template("secrets.html", name=current_user.name, logged_in=True)
 
 
 @app.route("/logout")
+@login_required
 def logout():
     logout_user()
     return redirect(url_for("home"))
 
 
 @app.route("/download")
+@login_required
 def download():
-    return send_from_directory(
-        directory="static/files", path=cheat_sheet.pdf, as_attachment=True
-    )
+    return send_from_directory("static", filename="files/cheat_sheet.pdf")
 
 
 if __name__ == "__main__":
