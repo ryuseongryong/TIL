b19ad1e88535acc778d77d12beb05e1bd0fa115d
ryuseongryong <ryuseongryong@gmail.com>
2023-01-27 23:59:49 +0900

    Add cards start

--- golang/udemy/cards/main.go
+++ golang/udemy/cards/main.go
@@ -6,3 +6,9 @@ func main() {
 	var card string = "Ace of Spades"
 	fmt.Println(card)
 }
+
+// basic Go types
+// bool true false
+// string "Hi" "Hows it going?"
+// int 0 -10000 9999
+// float64 10.001 0.009
