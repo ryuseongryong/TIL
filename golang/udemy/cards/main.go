ddf605cf10b734fee2dabd0de3c6946e4e555fdf
ryuseongryong <ryuseongryong@gmail.com>
2023-01-31 08:05:48 +0900

    Custom Type Declarations, Receiver Functions

--- golang/udemy/cards/main.go
+++ golang/udemy/cards/main.go
@@ -1,16 +1,10 @@
 package main
 
-import "fmt"
-
 func main() {
 	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
-
-	// fmt.Println(cards)
+	cards.print()
 }
 
 func newCard() string {
