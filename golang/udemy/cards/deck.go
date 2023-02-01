bdc16f54308897f7cb37af60c28e3e3f1c8a6dd1
ryuseongryong <ryuseongryong@gmail.com>
2023-02-01 21:42:00 +0900

    Add new Deck

--- golang/udemy/cards/deck.go
+++ golang/udemy/cards/deck.go
@@ -7,6 +7,22 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	cards := deck{}
+
+	cardSuits := []string{"Spades", "Diamonds", "Hearts", "Clubs"}
+	cardValues := []string{"Ace", "Two", "Three", "Four"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, value+" of "+suit)
+		}
+	}
+
+	return cards
+}
+
+// almost receiver is first single string
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
