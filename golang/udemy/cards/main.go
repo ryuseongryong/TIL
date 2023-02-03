2bad948e818ced65424022a384ac2ce7d42abf4e
ryuseongryong <ryuseongryong@gmail.com>
2023-02-03 23:09:32 +0900

    Deck to string

--- golang/udemy/cards/main.go
+++ golang/udemy/cards/main.go
@@ -1,12 +1,16 @@
 package main
 
+import "fmt"
+
 func main() {
-	cards := newDeck()
+	// cards := newDeck()
 
-	hand, remainingCards := deal(cards, 5)
+	// hand, remainingCards := deal(cards, 5)
 
-	hand.print()
-	remainingCards.print()
+	// hand.print()
+	// remainingCards.print()
+	greeting := "Hi therer!"
+	fmt.Println([]byte(greeting))
 }
 
 // basic Go types
