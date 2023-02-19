3755d8e9e16b60992149090b18e111fae1d57e2d
ryuseongryong <ryuseongryong@gmail.com>
2023-02-19 09:52:06 +0900

    Updating Struct Values

--- 02-Programming/golang/udemy/2_structs/main.go
+++ 02-Programming/golang/udemy/2_structs/main.go
@@ -8,6 +8,28 @@ type person struct {
 }
 
 func main() {
-	alex := person{firstName: "Alex", lastName: "Anderson"}
+	var alex person
+
+	alex.firstName = "Alex"
+	alex.lastName = "Anderson"
+
 	fmt.Println(alex)
+	fmt.Printf("%+v", alex)
 }
+
+// type     | default value
+// string   | 0
+// float    | 0
+// bool     | false
+
+// structs variable assignment 3ways
+// 1. 	by struct order
+//      e.g. alex := person{"Alex", "Anderson"}
+// 2. 	assign as key
+//      e.g. alex := person{firstName: "Alex", lastName: "Anderson"}
+// 3.   create variable and update values
+// 		e.g. var alex person
+//      	 alex.firstName = "Alex"
+//      	 alex.lastName = "Anderson"
+//      	 fmt.Println(alex)
+//      	 fmt.Printf("%+v", alex)
