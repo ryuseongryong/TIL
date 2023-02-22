87182eb2f57cfeebc5029225bd8d568c7fc18951
ryuseongryong <ryuseongryong@gmail.com>
2023-02-22 23:57:48 +0900

    structs with pointer

--- 02-Programming/golang/udemy/2_structs/main.go
+++ 02-Programming/golang/udemy/2_structs/main.go
@@ -21,12 +21,14 @@ func main() {
 			zipCode: 94000,
 		},
 	}
-	jim.updateName("jimmy")
+
+	jimPointer := &jim
+	jimPointer.updateName("jimmy")
 	jim.print()
 }
 
-func (p person) updateName(newFirstName string) {
-	p.firstName = newFirstName
+func (pointerToPerson *person) updateName(newFirstName string) {
+	(*pointerToPerson).firstName = newFirstName
 }
 
 func (p person) print() {
