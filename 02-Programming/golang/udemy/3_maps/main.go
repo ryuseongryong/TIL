09499f73914f4102a430a4215225857bd9830fce
ryuseongryong <ryuseongryong@gmail.com>
2023-03-02 22:02:17 +0900

    Manipulating Maps

--- 02-Programming/golang/udemy/3_maps/main.go
+++ 02-Programming/golang/udemy/3_maps/main.go
@@ -3,10 +3,18 @@ package main
 import "fmt"
 
 func main() {
-	colors := map[string]string{
-		"red":   "#ff0000",
-		"green": "#4bf745",
-	}
+	// var colors map[string]string
+
+	colors := make(map[string]string)
+
+	// colors := map[string]string{
+	// 	"red":   "#ff0000",
+	// 	"green": "#4bf745",
+	// }
+
+	colors["white"] = "#ffffff"
+
+	delete(colors, "white")
 
 	fmt.Println(colors)
 }
