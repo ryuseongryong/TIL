c193f0564039941b650602988594cb6c84a34581
ryuseongryong <ryuseongryong@gmail.com>
2023-03-05 23:15:10 +0900

    Iterating over Maps

--- 02-Programming/golang/udemy/3_maps/main.go
+++ 02-Programming/golang/udemy/3_maps/main.go
@@ -5,16 +5,23 @@ import "fmt"
 func main() {
 	// var colors map[string]string
 
-	colors := make(map[string]string)
+	// colors := make(map[string]string)
 
-	// colors := map[string]string{
-	// 	"red":   "#ff0000",
-	// 	"green": "#4bf745",
-	// }
+	colors := map[string]string{
+		"red":   "#ff0000",
+		"green": "#4bf745",
+		"white": "#ffffff",
+	}
 
-	colors["white"] = "#ffffff"
+	printMap(colors)
+	// colors["white"] = "#ffffff"
 
-	delete(colors, "white")
+	// delete(colors, "white")
 
-	fmt.Println(colors)
+}
+
+func printMap(c map[string]string) {
+	for color, hex := range c {
+		fmt.Println("Hex code for", color, "is", hex)
+	}
 }
