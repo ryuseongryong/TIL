0a3434968cb419ae17eaad30aa588b00e56de186
ryuseongryong <ryuseongryong@gmail.com>
2023-03-09 23:58:01 +0900

    Add chatbot description

---
+++ 02-Programming/golang/udemy/4_interfaces/main.go
@@ -0,0 +1,12 @@
+// mini chat bot program
+
+// type englishBot struct
+// func (englishBot) getGreeting() string
+// func printGreeting(eb englishBot)
+
+// type spanishBot struct
+// func (spanishBot) getGreeting() string
+// func printGreeting(sb spanishBot)
+
+// func getGreeting() : very differnet logic in each funcs
+// func printGreeting() : these will have identical logic 
