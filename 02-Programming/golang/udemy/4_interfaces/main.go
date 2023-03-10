b7229fb4c1e35ae87a86aa5a211c8b2a4404e50b
ryuseongryong <ryuseongryong@gmail.com>
2023-03-11 00:00:12 +0900

    Add interface problem

--- 02-Programming/golang/udemy/4_interfaces/main.go
+++ 02-Programming/golang/udemy/4_interfaces/main.go
@@ -9,4 +9,36 @@
 // func printGreeting(sb spanishBot)
 
 // func getGreeting() : very differnet logic in each funcs
-// func printGreeting() : these will have identical logic 
+// func printGreeting() : these will have identical logic
+
+package main
+
+import "fmt"
+
+type englishBot struct{}
+type spanishBot struct{}
+
+func main() {
+	eb := englishBot{}
+	sb := spanishBot{}
+
+	printGreeting(eb)
+	printGreeting(sb)
+}
+
+func printGreeting(eb englishBot) {
+	fmt.Println(eb.getGreeting())
+}
+
+func printGreeting(sb spanishBot) {
+	fmt.Println(sb.getGreeting())
+}
+
+func (englishBot) getGreeting() string {
+	// custom logic fo generating an english greeting
+	return "Hi There!"
+}
+
+func (spanishBot) getGreeting() string {
+	return "Hola!"
+}
