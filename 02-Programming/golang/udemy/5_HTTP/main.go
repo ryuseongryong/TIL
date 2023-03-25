057f712366ccdcc88a8d4866193b607bb4ef40d6
ryuseongryong <ryuseongryong@gmail.com>
2023-03-25 23:46:43 +0900

    Add log Writer

--- 02-Programming/golang/udemy/5_HTTP/main.go
+++ 02-Programming/golang/udemy/5_HTTP/main.go
@@ -7,6 +7,8 @@ import (
 	"os"
 )
 
+type logWriter struct{}
+
 func main() {
 	resp, err := http.Get("http://google.com")
 	if err != nil {
@@ -17,5 +19,13 @@ func main() {
 	// bs := make([]byte, 99999)
 	// resp.Body.Read(bs)
 	// fmt.Println(string(bs))
-	io.Copy(os.Stdout, resp.Body)
+	lw := logWriter{}
+
+	io.Copy(lw, resp.Body)
+}
+
+func (logWriter) Write(bs []byte) (int, error) {
+	fmt.Println(string(bs))
+	fmt.Println("Just wrote this many bytes:", len(bs))
+	return len(bs), nil
 }
