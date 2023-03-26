ffd931a37020fd12c3b64e354e16767ffccfcf26
ryuseongryong <ryuseongryong@gmail.com>
2023-03-26 23:59:56 +0900

    Add test shape

---
+++ 02-Programming/golang/udemy/5_HTTP/shape/main.go
@@ -0,0 +1,33 @@
+package main
+
+import "fmt"
+
+type shape interface {
+	getArea() float64
+}
+
+type triangle struct {
+	base   float64
+	height float64
+}
+
+type square struct {
+	sideLength float64
+}
+
+func (t triangle) getArea() float64 {
+	fmt.Println(0.5 * t.base * t.height)
+	return 0.5 * t.base * t.height
+}
+
+func (s square) getArea() float64 {
+	fmt.Println(s.sideLength * s.sideLength)
+	return s.sideLength * s.sideLength
+}
+
+func main() {
+	nt := triangle{
+		base:   4.5,
+		height: 5.5,
+	},
+}
