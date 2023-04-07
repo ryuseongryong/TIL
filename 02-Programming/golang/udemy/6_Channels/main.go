89cceb6400297914a3fca11c7b1274b90a72a911
ryuseongryong <ryuseongryong@gmail.com>
2023-04-08 00:22:22 +0900

    Add channel repeating routines

--- 02-Programming/golang/udemy/6_Channels/main.go
+++ 02-Programming/golang/udemy/6_Channels/main.go
@@ -29,11 +29,11 @@ func main() {
 func checkLink(link string, c chan string) {
 	_, err := http.Get(link)
 	if err != nil {
-		fmt.Println(link, "might be down!")
-		c <- "Might be dwon I think"
+		fmt.Println(link, "might be down!", err)
+		c <- link
 		return
 	}
 
 	fmt.Println(link, "is up!")
-	c <- "Yep its up"
+	c <- link
 }
