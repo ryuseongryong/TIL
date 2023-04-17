64ad231618330549f97268d5609c9389041d8d81
ryuseongryong <ryuseongryong@gmail.com>
2023-04-17 23:33:56 +0900

    go: package function

--- 02-Programming/golang/udemy2/CARDS/main.go
+++ 02-Programming/golang/udemy2/CARDS/main.go
@@ -1,15 +1,11 @@
 package main
 
-import "fmt"
-
 func main() {
 	// var card string = "Ace of Spades"
 	cards := deck{"Ace of Diamonds", newCard()}
 	cards = append(cards, "Six of Spades")
 
-	for i, card := range cards {
-		fmt.Println(i, card)
-	}
+	cards.print()
 }
 
 func newCard() string {
