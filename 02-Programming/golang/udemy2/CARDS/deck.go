666ff33a9b3edd7542048d94fa25c90794cb5add
ryuseongryong <ryuseongryong@gmail.com>
2023-04-18 23:48:51 +0900

    go: for loop with slices

--- 02-Programming/golang/udemy2/CARDS/deck.go
+++ 02-Programming/golang/udemy2/CARDS/deck.go
@@ -4,6 +4,19 @@ import "fmt"
 
 type deck []string
 
+func newDeck() deck {
+	// cards := deck{"Ace of Spades", "Two of Spades"}
+	cards := deck{}
+	cardSuits := []string{"Spades", "Hearts", "Diamonds"}
+	cardValues := []string{"Ace", "Two", "Three"}
+
+	for _, suit := range cardSuits {
+		for _, value := range cardValues {
+			cards = append(cards, suit+" of "+value)
+		}
+	}
+}
+
 func (d deck) print() {
 	for i, card := range d {
 		fmt.Println(i, card)
