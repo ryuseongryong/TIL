a89969194287edefb6ea1c3ef47c567662648cfc
ryuseongryong <ryuseongryong@gmail.com>
2023-04-21 23:54:46 +0900

    go: write file

--- 02-Programming/golang/udemy2/CARDS/deck.go
+++ 02-Programming/golang/udemy2/CARDS/deck.go
@@ -1,6 +1,10 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"strings"
+)
 
 type deck []string
 
@@ -26,4 +30,12 @@ func (d deck) print() {
 
 func deal(d deck, handSize int) (deck, deck) {
 	return d[:handSize], d[handSize:]
+}
+
+func (d deck) toString() string {
+	return strings.Join([]string(d), ",")
+}
+
+func (d deck) saveToFile(filename string) error {
+	return ioutil.WriteFile(filename, []byte(d.toString()), 0666)
 }
