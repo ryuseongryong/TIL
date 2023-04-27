1dce487408f0cb7f51b896d4445c6a9e04bade0a
ryuseongryong <ryuseongryong@gmail.com>
2023-04-27 23:16:31 +0900

    go: Add go test2

--- 02-Programming/golang/udemy2/CARDS/deck.go
+++ 02-Programming/golang/udemy2/CARDS/deck.go
@@ -19,7 +19,7 @@ func newDeck() deck {
 
 	for _, suit := range cardSuits {
 		for _, value := range cardValues {
-			cards = append(cards, suit+" of "+value)
+			cards = append(cards, value+" of "+suit)
 		}
 	}
 	return cards
