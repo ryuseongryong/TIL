c1877793f43f85fa5377e6361653d73cd6d404d3
ryuseongryong <ryuseongryong@gmail.com>
2023-05-09 23:00:18 +0900

    rust: integers

--- 02-Programming/rust/intro.rs
+++ 02-Programming/rust/intro.rs
@@ -12,8 +12,8 @@ Rust types(https://dhghomon.github.io/easy_rust/Chapter_7.html)
 Integers
 - primitive types
 - integers, char
-- Signed integers : i8, i16, i32, i64, i128, isize
-- Unsigned integers : u8, u16, u32, u64, u128, usize
+- Signed integers : i8, i16, i32, i64, i128, isize, + and -
+- Unsigned integers : u8, u16, u32, u64, u128, usize, only +
 - 숫자는 bits 수를 뜻함. 8bits = 1byte. u8 <= 255, u16 <= 65535, u128 <= 340282366920938463463374607431768211455
 - isize, usize는 컴퓨터의 bits 수. e.g. 32bits computer의 isize, usize는 i32, u32.
 - byte수가 적을수록 처리 속도가 빠름. 정수의 유형이 다른 것이 이 성능이 이유인 것도 있지만, 다른 용도로 사용할 수도 있음.
@@ -60,5 +60,12 @@ fn main() {
     let my_number: u8 = 100; //  change my_number to my_number: u8
     println!("{}", my_number as char);
 }
+
+fn main() {
+    let my_number: u8 = 100; // 255
+    let my_other_number: u16 = 50;
+    let third_number = my_number + my_other_number;
+}
+--> mismatched types
 
 */
