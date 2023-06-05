827a8c11c48d92664c35c6710713e18431fb3089
ryuseongryong <ryuseongryong@gmail.com>
2023-06-05 20:45:09 +0900

    rust: 04 cli app - basic type

--- 02-Programming/rust/udemy/04-cliapp.rs
+++ 02-Programming/rust/udemy/04-cliapp.rs
@@ -3,3 +3,24 @@
 // - Basic Data Types
 // - Standard Library
 // - Memory Ownership
+
+// Basic Data Types
+// - Booleans
+// - Characters
+// - Integers
+// - Floats
+
+// Integers
+// u8 : 무부호 정수, 양수만 가능
+// i8 : 유부호 정수, 양수 또는 음수 가능
+// u8 : 8bit 숫자 또는 1Byte. 값의 범위는 128bit까지 가능
+// usize, isize : 아키텍처 종속 타입으로 32bit 아키텍처 환경에서는 32bit, 64bit 아키텍처 환경에서는 64bit.
+
+// Floats
+// f32, f64 : 부동소수점 숫자 타입. 32bit, 64bit 두 가지가 있음
+
+// Boolean
+// True or False, 1Byte, bool keyword를 사용하여 선언함.
+
+// Char
+// 하나의 유니코드 값을 갖는다. 중요한 점은 4Byte 크기라는 것이다. 그래서 ASCII문자 집합을 char로 저장한다면 메모리 낭비가 된다.
