ffe6418f8a75b66e7814f79c562ad56e760f932c
ryuseongryong <ryuseongryong@gmail.com>
2023-07-02 12:28:27 +0900

    Algorithm 덧칠하기

---
+++ 01-Algorithm/덧칠하기.py
@@ -0,0 +1,23 @@
+def solution(n, m, section):
+    rolling = m
+
+    count = {target: 0 for target in section}
+    answer = 0
+
+    for i in count:
+        # print(i, count[i])
+        if count[i] == 0:
+            for j in range(i, i + rolling):
+                # print(j)
+                if j in count:
+                    count.update({j: 1})
+            answer += 1
+        #         print(count)
+        # print("after", section)
+        print("after", count)
+
+    print(answer)
+    return answer
+
+
+solution(8, 4, [2, 3, 6])
