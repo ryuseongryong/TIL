4193a06b81600ea355eb0774ec4331be07f2cece
ryuseongryong <ryuseongryong@gmail.com>
2023-07-16 23:38:41 +0900

    rust: string1

--- 02-Programming/rust/udemy/server/src/main.rs
+++ 02-Programming/rust/udemy/server/src/main.rs
@@ -1,6 +1,11 @@
 fn main() {
-    let server = Server::new("127.0.0.1:8080");
-    server.run();
+    let string = String::from("127.0.0.1:8080");
+    let string_slice = &string[10..];
+
+    dbg!(&string);
+    dbg!(string_slice);
+    // let server = Server::new("127.0.0.1:8080");
+    // server.run();
 }
 
 struct Server {
