1e92c5ee59b52cd21ac1c891d0ade74b519599ca
ryuseongryong <ryuseongryong@gmail.com>
2023-07-28 23:55:27 +0900

    rust: tcp connection

--- 02-Programming/rust/udemy/server/src/server.rs
+++ 02-Programming/rust/udemy/server/src/server.rs
@@ -1,3 +1,5 @@
+use std::net::TcpListener;
+
 pub struct Server {
     addr: String,
 }
@@ -11,5 +13,7 @@ impl Server {
 
     pub fn run(self) {
         println!("Listening on {}", self.addr)
+
+        let listener = TcpListener::bind(self.addr);
     }
 }
