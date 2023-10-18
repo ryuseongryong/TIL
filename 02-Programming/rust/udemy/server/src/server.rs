40bb0336fc664b977b3472ad873815e396d8c0b9
ryuseongryong <ryuseongryong@gmail.com>
2023-10-19 00:40:08 +0900

    rust: trait and type convert

--- 02-Programming/rust/udemy/server/src/server.rs
+++ 02-Programming/rust/udemy/server/src/server.rs
@@ -1,3 +1,6 @@
+use crate::http::Request;
+use std::convert::TryFrom;
+use std::convert::TryInto;
 use std::io::Read;
 use std::net::TcpListener;
 pub struct Server {
@@ -23,6 +26,12 @@ impl Server {
                     match stream.read(&mut buffer) {
                         Ok(_) => {
                             println!("Received a request: {}", String::from_utf8_lossy(&buffer))
+
+                            match Request::try_from(&buffer[..]) {
+                                Ok(request) => {},
+                                Err(e) => println!("Failed to parse a request: {}", e)
+                            }
+                            
                         }
                         Err(e) => println!("Failed to read from connection: {}", e),
                     }
