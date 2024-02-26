e6a32dbe9fa8a05438e89669d730005caca60b35
ryuseongryong <ryuseongryong@gmail.com>
2024-02-26 23:11:48 +0900

    rust: Convert Option into result

--- 02-Programming/rust/udemy/server/src/http/request.rs
+++ 02-Programming/rust/udemy/server/src/http/request.rs
@@ -1,5 +1,7 @@
 use std::str::Utf8Error;
 use std::str;
+use crate::http::method;
+
 use super::method::Method;
 use std::convert::TryFrom;
 use std::error::Error;
@@ -18,13 +20,26 @@ impl TryFrom<&[u8]> for Request {
     fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
         let request = str::from_utf8(buf)?;
 
+        match get_next_word(request) {
+            Some((method, request)) => {},
+            None => return Err(ParseError::InvalidRequest),
+        }
+
+        let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+
+        if protocol != "HTTP/1.1" {
+            return Err(ParseError::InvalidProtocol);
+        }
+
         unimplemented!()
     }
 }
 
 fn get_next_word(request: &str) -> Option<(&str, &str)> {
     for (i, c) in request.chars().enumerate() {
-        if c == ' ' {
+        if c == ' ' || c == '\r' {
             return Some((&request[..i], &request[i + i..]));
         }
     }
