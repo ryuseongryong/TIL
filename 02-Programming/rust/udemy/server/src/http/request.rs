5cd69338c17df54a0a9112e0a5716cf199ba9bc4
ryuseongryong <ryuseongryong@gmail.com>
2024-02-28 23:39:02 +0900

    rust: if-let chain

--- 02-Programming/rust/udemy/server/src/http/request.rs
+++ 02-Programming/rust/udemy/server/src/http/request.rs
@@ -26,7 +26,7 @@ impl TryFrom<&[u8]> for Request {
         }
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
-        let (path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
+        let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (protocol, _) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
 
         if protocol != "HTTP/1.1" {
@@ -34,6 +34,31 @@ impl TryFrom<&[u8]> for Request {
         }
 
         let method: Method = method.parse()?;
+
+        let mut query_string = None;
+        // 1. match
+        // match path.find('?') {
+        //     Some(i) => {
+        //         query_string = Some(&path[i + 1..]);
+        //         path = &path[..i];
+        //     }
+        //     None => {}
+        // }
+        
+        // 2. every varients
+        // let q = path.find('?');
+        // if q.is_some() {
+        //     let i = q.unwrap();
+        //     query_string = Some(&path[i + 1..]);
+        //     path = &path[..i];
+        // }
+
+
+        // 3. target varient
+        if let Some(i) = path.find('?') {
+            query_string = Some(&path[i + 1..]);
+            path = &path[..i];    
+        }
 
         unimplemented!()
     }
