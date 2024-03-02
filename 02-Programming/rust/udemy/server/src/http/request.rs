c29c317f87ed50bfdc96d7aeb65243659fae0138
ryuseongryong <ryuseongryong@gmail.com>
2024-03-02 23:57:48 +0900

    rust: lifetime2 error

--- 02-Programming/rust/udemy/server/src/http/request.rs
+++ 02-Programming/rust/udemy/server/src/http/request.rs
@@ -7,23 +7,18 @@ use std::convert::TryFrom;
 use std::error::Error;
 use std::fmt::{Result as FmtResult, Display, Formatter, Debug};
 
-pub struct Request {
-    path: &str,
-    query_string: Option<&str>,
+pub struct Request<'buf> {
+    path: &'buf str,
+    query_string: Option<&'buf str>,
     method: Method,
 }
 
-impl TryFrom<&[u8]> for Request {
+impl<'buf> TryFrom<&'buf [u8]> for Request<'buf> {
     type Error = ParseError;
 
     //? GET /search?name=abc&sort=1 HTTP/1.1
-    fn try_from(buf: &[u8]) -> Result<Self, Self::Error> {
+    fn try_from<(buf: &'buf [u8]) -> Result<Request<'buf>, Self::Error> {
         let request = str::from_utf8(buf)?;
-
-        match get_next_word(request) {
-            Some((method, request)) => {},
-            None => return Err(ParseError::InvalidRequest),
-        }
 
         let (method, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
         let (mut path, request) = get_next_word(request).ok_or(ParseError::InvalidRequest)?;
@@ -43,7 +38,7 @@ impl TryFrom<&[u8]> for Request {
         }
 
         Ok(Self {
-            path: path,
+            path,
             query_string,
             method,
         })
